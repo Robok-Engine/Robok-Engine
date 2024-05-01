daa8cc7c32a065a2be43edbf60cba7a278e1bc8f
aquilesTrindade <sempedra075@gmail.com>
2024-05-01 19:43:12 -0300

    fix errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,31 +1,52 @@
+
 plugins {
     id("com.android.application")
+    id("kotlin-android")
 }
 
 android {
-    compileSdk = 34
     namespace = "com.trindade.gamide"
-	
+    compileSdk = 34
+    
     defaultConfig {
         applicationId = "com.trindade.gamide"
         minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        
+        vectorDrawables { 
+            useSupportLibrary = true
+        }
     }
-
-    buildTypes {
-            isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-    }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        
+    }
+    
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "11"
 }
 
 dependencies {
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-	implementation ("com.google.android.material:material:1.9.0")
-	implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    
 }
