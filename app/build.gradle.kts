8ef6dc366a68b180dcceb07e482d1307502ba350
aquilesTrindade <sempedra075@gmail.com>
2024-05-07 12:42:52 -0300

    fix errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     
     defaultConfig {
         applicationId = "com.trindade.gamide"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
