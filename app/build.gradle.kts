1bdbc3a9d6b1c043cbfd76df09a9121236ceaba7
aquilesTrindade <sempedra075.com>
2024-06-20 20:50:41 -0300

    update Robok Language version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 
 dependencies {
 
-    val robok_version = "1.0.2-Fixes"
+    val robok_version = "2.0.0"
     val kotlin_version = "2.0.0"
     val kotlin_coroutines_version = "1.9.0-RC"
     val compose_version = "2024.05.00"
