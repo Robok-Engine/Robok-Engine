37f5d546c1ea6936740913df385d61ece4221b5c
devsuay <devsuay@gmail.com>
2024-06-21 03:12:50 -0300

    Use Implementatiom of RobokLanguage again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 
 dependencies {
 
+    val robok_language_version = "2.0.0-fix02"
     val kotlin_version = "2.0.0"
     val kotlin_coroutines_version = "1.9.0-RC"
     val compose_version = "2024.05.00"
@@ -100,5 +101,5 @@ dependencies {
     implementation("androidx.activity:activity:$activity_version")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     
-    implementation(project(":language"))
+    implementation("com.github.Robok-Foundation:Robok-Language:$robok_language_version")
 }
