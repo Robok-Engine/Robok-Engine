f769e711ddb025778275b9d0556a21469a08af04
devsuay <devsuay@gmail.com>
2024-06-21 02:33:23 -0300

    fixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,24 @@
-pluginManagement {
-  repositories {
-    gradlePluginPortal()
-    google()
-    mavenCentral()
-  }
-}
-
-dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-    maven(url = "https://jitpack.io")
-  }
-}
-
-rootProject.name = "GamIDE"
+rootProject.name = "Robok-IDE"
 
 var root = "language"
 
 include(":app")
 include(":language")
 project(":language").projectDir = file("$root${File.separator}language")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
