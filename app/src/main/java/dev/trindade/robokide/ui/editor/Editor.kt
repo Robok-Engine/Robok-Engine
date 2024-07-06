02e80c7494730509e60803047c3e88344504e083
aquilesTrindade <sempedra075@gmail.com>
2024-07-06 15:31:06 -0300

    fix highlight editor

--- app/src/main/java/dev/trindade/robokide/ui/editor/Editor.kt
+++ app/src/main/java/dev/trindade/robokide/ui/editor/Editor.kt
@@ -1,29 +1,35 @@
 package dev.trindade.robokide.ui.editor
 
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.EditText
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
-fun HighlightingEditor(syntaxType: String) {
+fun HighlightingEditor(
+    value: String,
+    onValueChange: (String) -> Unit,
+    syntaxType: String
+) {
     val context = LocalContext.current
-    var text by remember { mutableStateOf("") }
 
     AndroidView(factory = { ctx ->
         EditText(ctx).apply {
+            setText(value)
             addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
                 override fun afterTextChanged(s: Editable?) {
-                    text = s.toString()
+                    s?.let {
+                        onValueChange(it.toString())
+                    }
                 }
             })
             SimpleHighlighter(this, syntaxType)
         }
     }, update = {
-        it.setText(text)
+        if (it.text.toString() != value) {
+            it.setText(value)
+        }
     })
 }
