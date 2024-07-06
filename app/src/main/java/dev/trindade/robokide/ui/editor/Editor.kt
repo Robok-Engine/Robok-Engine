e0282e7706bb9531198db9e4d3a9b8641ad19bf1
aquilesTrindade <sempedra075@gmail.com>
2024-07-06 15:36:02 -0300

    fix highlight editor

--- app/src/main/java/dev/trindade/robokide/ui/editor/Editor.kt
+++ app/src/main/java/dev/trindade/robokide/ui/editor/Editor.kt
@@ -1,9 +1,25 @@
 package dev.trindade.robokide.ui.editor
 
-import android.widget.EditText
+import androidx.activity.*
+import androidx.navigation.*
+import androidx.compose.ui.*
+import androidx.compose.ui.unit.*
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.draw.*
+import androidx.activity.compose.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.layout.*
+import androidx.compose.material3.*
+import androidx.navigation.compose.*
+import androidx.compose.foundation.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.text.input.*
+import androidx.compose.material.icons.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.viewinterop.*
+import dev.trindade.robokide.ui.syntax.*
 
 @Composable
 fun HighlightingEditor(
