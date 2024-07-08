c2605538575f834cbe37ef01609ad6997cd5f00f
Aquiles Trindade <147993300+aquilesTrindade@users.noreply.github.com>
2024-07-08 20:17:41 -0300

    fix: use output in constructor

--- app/src/main/java/dev/trindade/robokide/ui/fragments/editor/EditorFragment.kt
+++ app/src/main/java/dev/trindade/robokide/ui/fragments/editor/EditorFragment.kt
@@ -52,9 +52,8 @@ class EditorFragment : Fragment() {
             }
 
             override fun onOutput(output: String) {
-                val outputFragment = OutputFragment()
-                outputFragment.addOutput(outputFragment.requireContext(), output)
-                
+                val outputFragment = OutputFragment(output)
+         
                 Snackbar.make(terminal.terminal, R.string.message_compiled, Snackbar.LENGTH_LONG)
                     .setAction(R.string.go_to_outputs) {
                         openFragment(outputFragment)
