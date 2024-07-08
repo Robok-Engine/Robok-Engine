d8b7918c1d8f9cf6df7700f1364de0470b9cf4a5
aquilesTrindade <sempedra075@gmail.com>
2024-07-07 23:52:45 -0300

    fix: syntaxtType npt found

--- app/src/main/java/dev/trindade/robokide/ui/editor/Editor.kt
+++ app/src/main/java/dev/trindade/robokide/ui/editor/Editor.kt
@@ -24,7 +24,7 @@ class Editor : EditText {
     private fun init(context: Context, attrs: AttributeSet?) {
         if (attrs != null) {
             val typedArray = context.obtainStyledAttributes(attrs, R.styleable.Editor)
-            val syntaxType = typedArray.getString(R.styleable.Editor_syntaxtType)
+            val syntaxType = typedArray.getString(R.styleable.Editor_syntaxType)
             typedArray.recycle()
 
             syntaxType?.let {
