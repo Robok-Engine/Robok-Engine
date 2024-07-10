89b9b7f6368534c5faa17da50a15501fac6ab872
aquilesTrindade <sempedra075@gmail.com>
2024-07-10 18:40:23 -0300

    fix: app

--- app/src/main/java/dev/trindade/robokide/ui/components/editor/themes/RobokDarcula.kt
+++ app/src/main/java/dev/trindade/robokide/ui/components/editor/themes/RobokDarcula.kt
@@ -7,9 +7,11 @@ import androidx.core.content.ContextCompat
 import io.github.rosemoe.sora.widget.EditorColorScheme
 import io.github.rosemoe.sora.widget.schemes.SchemeDarcula
 
+import dev.trindade.robokide.R
+
 class RobokDarcula(context: Context) : SchemeDarcula() {
     init {
-        val customColor = ContextCompat.getColor(context, R.color.md_theme_background)
-        colorPool.setColor(EditorColorScheme.WHOLE_BACKGROUND, customColor)
+        val backgroundColor = ContextCompat.getColor(context, R.color.md_theme_background)
+        colorPool.setColor(EditorColorScheme.WHOLE_BACKGROUND, backgroundColor)
     }
 }
