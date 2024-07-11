07e5ecc037baf14f97997de29baa487cf3e0c905
aquilesTrindade <sempedra075@gmail.com>
2024-07-11 01:25:41 -0300

    change colors

--- app/src/main/java/dev/trindade/robokide/ui/components/editor/schemes/SchemeRobok.kt
+++ app/src/main/java/dev/trindade/robokide/ui/components/editor/schemes/SchemeRobok.kt
@@ -23,8 +23,8 @@ class SchemeRobok(context: Context) : SchemeDarcula() {
     init {
         this.context = context
         setColor(EditorColorScheme.WHOLE_BACKGROUND, getAttrColor(android.R.attr.colorBackground)) // set the background color
-        setColor(EditorColorScheme.CURRENT_LINE, getAttrColor(com.google.android.material.R.attr.colorOnBackground)) // set the current line color
-        setColor(EditorColorScheme.LINE_NUMBER_PANEL, getAttrColor(com.google.android.material.R.attr.colorSurfaceContainerHigh)) // set color bar for line numbers
+        setColor(EditorColorScheme.CURRENT_LINE, getAttrColor(android.R.attr.colorBackground)) // set the current line color
+        setColor(EditorColorScheme.LINE_NUMBER_PANEL, getAttrColor(android.R.attr.colorBackground)) // set color bar for line numbers
     }
 
     private fun getAttrColor(@AttrRes resId: Int): Int {
