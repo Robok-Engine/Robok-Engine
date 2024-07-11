e032111734d69c5b545b9f85eaab02a8e16238f9
aquilesTrindade <sempedra075@gmail.com>
2024-07-11 01:08:41 -0300

    fix: context not found

--- app/src/main/java/dev/trindade/robokide/ui/components/editor/schemes/SchemeRobok.kt
+++ app/src/main/java/dev/trindade/robokide/ui/components/editor/schemes/SchemeRobok.kt
@@ -17,7 +17,11 @@ import io.github.rosemoe.sora.widget.schemes.EditorColorScheme
 import dev.trindade.robokide.R
 
 class SchemeRobok(context: Context) : SchemeDarcula() {
+
+    val context : Context
+
     init {
+        this.context = context
         setColor(EditorColorScheme.WHOLE_BACKGROUND, getAttrColor(android.R.attr.colorBackground)) // set the background color
         setColor(EditorColorScheme.CURRENT_LINE, getAttrColor(com.google.android.material.R.attr.colorOnBackground)) // set the current line color
         setColor(EditorColorScheme.LINE_NUMBER_PANEL, getAttrColor(com.google.android.material.R.attr.colorSurfaceContainerHigh)) // set color bar for line numbers
