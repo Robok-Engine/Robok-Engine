8d43dcb7ee6bf060ccd492cda8796b9f1049c0d4
aquilesTrindade <sempedra075@gmail.com>
2024-07-11 00:58:32 -0300

    improve Robok Theme

--- app/src/main/java/dev/trindade/robokide/ui/components/editor/themes/RobokDarcula.kt
+++ app/src/main/java/dev/trindade/robokide/ui/components/editor/themes/RobokDarcula.kt
@@ -3,23 +3,33 @@ package dev.trindade.robokide.ui.components.editor.themes
 /* 
  *  Class responsible for changing the colors of the Darcula Theme.
 */
+
 import android.content.Context
 import android.util.TypedValue
+
+import androidx.annotation.AttrRes
+
 import androidx.core.content.ContextCompat
-import io.github.rosemoe.sora.widget.schemes.EditorColorScheme
+
 import io.github.rosemoe.sora.widget.schemes.SchemeDarcula
+import io.github.rosemoe.sora.widget.schemes.EditorColorScheme
+
+import dev.trindade.robokide.R
 
 class RobokDarcula(context: Context) : SchemeDarcula() {
     init {
+        setColor(EditorColorScheme.WHOLE_BACKGROUND, getColor(android.R.attr.colorBackground)) // set the background color
+        setColor(EditorColorScheme.CURRENT_LINE, getColor(R.attr.colorOnBackground)) // set the current line color
+        setColor(EditorColorScheme.LINE_NUMBER_PANEL, getColor(R.attr.colorSurfaceContainerHigh)) // set color bar for line numbers
+    }
+
+    private fun getColor(@AttrRes resId: Int): Int {
         val typedValue = TypedValue()
-        context.theme.resolveAttribute(android.R.attr.colorBackground, typedValue, true)
-        
-        val backgroundColor = if (typedValue.resourceId != 0) {
+        context.theme.resolveAttribute(resId, typedValue, true)
+        return if (typedValue.resourceId != 0) {
             ContextCompat.getColor(context, typedValue.resourceId)
         } else {
             typedValue.data
         }
-        
-        setColor(EditorColorScheme.WHOLE_BACKGROUND, backgroundColor)
     }
 }
