7eb1282ebc5e963f25fc78566983a98cc5d54a6c
aquilesTrindade <sempedra075@gmail.com>
2024-07-10 22:08:43 -0300

    improve RbkTextField

--- app/src/main/java/dev/trindade/robokide/ui/components/textfield/RbkTextField.kt
+++ app/src/main/java/dev/trindade/robokide/ui/components/textfield/RbkTextField.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 import dev.trindade.robokide.databinding.LayoutRbkTextfieldBinding
 
 class RbkTextField @JvmOverloads constructor(
@@ -16,10 +15,26 @@ class RbkTextField @JvmOverloads constructor(
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
     private val binding: LayoutRbkTextfieldBinding = LayoutRbkTextfieldBinding.inflate(
-        LayoutInflater.from(context), 
-        this, 
+        LayoutInflater.from(context),
+        this,
         true
     )
+
+    init {
+        // Read custom attributes from XML
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.RbkTextField, defStyleAttr, 0)
+        
+        // Apply attributes to components
+        binding.background.hint = typedArray.getString(R.styleable.RbkTextField_rbkTextFieldHint)
+        binding.background.placeholderText = typedArray.getString(R.styleable.RbkTextField_rbkTextFieldPlaceholderText)
+        val startIconDrawableRes = typedArray.getResourceId(R.styleable.RbkTextField_rbkTextFieldStartIconDrawable, 0)
+        if (startIconDrawableRes != 0) {
+            binding.background.startIconDrawable = ContextCompat.getDrawable(context, startIconDrawableRes)
+        }
+        
+        // Always recycle the TypedArray
+        typedArray.recycle()
+    }
 
     var hint: CharSequence?
         get() = binding.background.hint
@@ -44,7 +59,4 @@ class RbkTextField @JvmOverloads constructor(
         set(value) {
             binding.edittext.setText(value)
         }
-
-    init {
-    }
 }
