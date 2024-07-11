0c73d90c9f857686959b290817cdaa3d7f08a630
aquilesTrindade <sempedra075@gmail.com>
2024-07-11 01:05:34 -0300

    fix: attrs not found

--- app/src/main/java/dev/trindade/robokide/ui/components/editor/schemes/SchemeRobok.kt
+++ app/src/main/java/dev/trindade/robokide/ui/components/editor/schemes/SchemeRobok.kt
@@ -18,12 +18,12 @@ import dev.trindade.robokide.R
 
 class SchemeRobok(context: Context) : SchemeDarcula() {
     init {
-        setColor(EditorColorScheme.WHOLE_BACKGROUND, getColor(android.R.attr.colorBackground)) // set the background color
-        setColor(EditorColorScheme.CURRENT_LINE, getColor(R.attr.colorOnBackground)) // set the current line color
-        setColor(EditorColorScheme.LINE_NUMBER_PANEL, getColor(R.attr.colorSurfaceContainerHigh)) // set color bar for line numbers
+        setColor(EditorColorScheme.WHOLE_BACKGROUND, getAttrColor(android.R.attr.colorBackground)) // set the background color
+        setColor(EditorColorScheme.CURRENT_LINE, getAttrColor(com.google.android.material.R.attr.colorOnBackground)) // set the current line color
+        setColor(EditorColorScheme.LINE_NUMBER_PANEL, getAttrColor(com.google.android.material.R.attr.colorSurfaceContainerHigh)) // set color bar for line numbers
     }
 
-    private fun getColor(@AttrRes resId: Int): Int {
+    private fun getAttrColor(@AttrRes resId: Int): Int {
         val typedValue = TypedValue()
         context.theme.resolveAttribute(resId, typedValue, true)
         return if (typedValue.resourceId != 0) {
