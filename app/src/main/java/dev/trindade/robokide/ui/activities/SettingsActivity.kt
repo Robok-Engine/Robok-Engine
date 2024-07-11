bf2f254533420b3dd32400bcebe5c0d256fadead
aquilesTrindade <sempedra075@gmail.com>
2024-07-11 03:40:07 -0300

    fix settings

--- app/src/main/java/dev/trindade/robokide/ui/activities/SettingsActivity.kt
+++ app/src/main/java/dev/trindade/robokide/ui/activities/SettingsActivity.kt
@@ -3,19 +3,24 @@ package dev.trindade.robokide.ui.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import com.google.android.material.appbar.MaterialToolbar
 
 import dev.trindade.robokide.R
+import dev.trindade.robokide.databinding.ActivitySettingsBinding
 import dev.trindade.robokide.ui.fragments.settings.SettingsFragment
+import dev.trindade.robokide.utils.getBackPressedClickListener
 
 class SettingsActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         navigationBack(binding.toolbar)
         if (savedInstanceState == null) {
             val fragment = SettingsFragment()
-            
             supportFragmentManager.commit {
                 replace(R.id.settings_fragment_container, fragment)
             }
@@ -23,6 +28,12 @@ class SettingsActivity : AppCompatActivity() {
     }
     
     fun setToolbarTitle(value: String) {
-        binding.toobar.setTitle(value)
+        binding.toolbar.title = value
+    }
+    
+    private fun navigationBack(toolbar: MaterialToolbar) {
+        toolbar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
