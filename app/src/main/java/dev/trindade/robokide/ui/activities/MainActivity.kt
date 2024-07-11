646fd31ca31a65ff1ed57cbf0723f4f21e4b7869
aquilesTrindade <sempedra075@gmail.com>
2024-07-11 12:57:04 -0300

    fix: ) expected

--- app/src/main/java/dev/trindade/robokide/ui/activities/MainActivity.kt
+++ app/src/main/java/dev/trindade/robokide/ui/activities/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : RobokActivity() {
         setContentView(R.layout.activity_main)
         
         if (savedInstanceState == null) {
-            openFragment(HomeFragment(MaterialSharedAxis.Y)
+            openFragment(HomeFragment(MaterialSharedAxis.Y))
         }
         
     }
