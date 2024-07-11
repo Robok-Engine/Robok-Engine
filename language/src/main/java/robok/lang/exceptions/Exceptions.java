65c5809ef5308f9ec3bce0a0c7ffc561ce4f5e7e
Aquiles Trindade <147993300+aquilesTrindade@users.noreply.github.com>
2024-07-11 00:16:00 -0300

    remove no-sense change

--- language/src/main/java/robok/lang/exceptions/Exceptions.java
+++ language/src/main/java/robok/lang/exceptions/Exceptions.java
@@ -2,6 +2,6 @@ package robok.lang.exceptions;
 
 public class Exceptions {
 
-     public static final String NO_METHOD_FOUND = "NoMethodFound.";
+     public static final String NO_METHOD_FOUND = "NoMethodFound";
 
 }
