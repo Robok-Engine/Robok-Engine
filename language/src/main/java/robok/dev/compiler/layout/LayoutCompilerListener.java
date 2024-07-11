6eb5eb72ce51263861b17e3c0cca936af60dedd5
aquilesTrindade <sempedra075@gmail.com>
2024-07-11 14:23:16 -0300

    remove language

--- language/src/main/java/robok/dev/compiler/layout/LayoutCompilerListener.java
+++
@@ -1,5 +0,0 @@
-package robok.dev.compiler.layout;
-
-public interface LayoutCompilerListener {
-	public void onCompiled(String logs);
-}
--- language/src/main/java/robok/lang/exceptions/Exceptions.java
+++
@@ -1,7 +0,0 @@
-package robok.lang.exceptions;
-
-public class Exceptions {
-
-     public static final String NO_METHOD_FOUND = "NoMethodFound";
-
-}
