51e8092cb1c9885bd4d8cfaf463276eba10ae455
aquilesTrindade <sempedra075@gmail.com>
2024-07-14 02:41:28 -0300

    add TeamMember adapter and RecyclerView

---
+++ app/src/main/java/dev/trindadedev/robokide/models/about/TeamMember.kt
@@ -0,0 +1,8 @@
+package dev.trindadedev.robokide.models.about
+
+data class TeamMember(
+    val profileIcon: String,
+    val name: String,
+    val description: String,
+    val socialLink: String
+)
