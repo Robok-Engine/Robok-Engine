aad77f72e35f694c51c97c29dbe3774223d81548
aquilesTrindade <sempedra075@gmail.com>
2024-07-14 22:37:49 -0300

    chore: update trindade-util to 3.0.q

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     val okhttp3Version = "4.12.0"
     val activityVersion = "1.9.0"
     val glideVersion = "4.16.0"
-    val trindadeutilVersion = "3.0.0"
+    val trindadeutilVersion = "3.0.1"
     
     val editorGroupId = "io.github.Rosemoe.sora-editor"
     
