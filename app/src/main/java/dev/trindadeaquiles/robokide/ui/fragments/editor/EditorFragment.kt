19edb93b7a45ef33064868177113d6d8a4c2b3e8
ThDev-only <rthiarley@gmail.com>
2024-07-17 01:10:00 -0300

    Progress Dots test 10

--- app/src/main/java/dev/trindadeaquiles/robokide/ui/fragments/editor/EditorFragment.kt
+++ app/src/main/java/dev/trindadeaquiles/robokide/ui/fragments/editor/EditorFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 
+import androidx.core.content.ContextCompat
+
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.transition.MaterialSharedAxis
