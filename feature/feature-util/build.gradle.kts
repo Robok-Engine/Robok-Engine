4ab777903b63ad9c0e39230e6d5d10d1206620fd
aquilesTrindade <sempedra075@gmail.com>
2024-08-03 21:59:49 -0300

    refactor: some refactors

--- feature/feature-util/build.gradle.kts
+++ feature/feature-util/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.2")
     
     implementation(project(":feature:feature-res:strings"))
 }
