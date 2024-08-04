da4667241f1877e37e7cb8140af0d68e5d8dba33
aquilesTrindade <sempedra075@gmail.com>
2024-08-04 17:00:30 -0300

    feat: add branchName and commitHash in appVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -9,19 +11,19 @@ plugins {
 android {
     namespace = "org.gampiot.robokide"
     compileSdk = 34
-    
+
     defaultConfig {
         applicationId = "org.gampiot.robokide"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
-        
-        vectorDrawables { 
+        versionName = getVersionName()
+
+        vectorDrawables {
             useSupportLibrary = true
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -45,7 +47,7 @@ android {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
-    
+
     signingConfigs {
         getByName("debug") {
             storeFile = file(layout.buildDirectory.dir("../testkey.keystore"))
@@ -67,29 +69,29 @@ dependencies {
     val kotlinVersion = "2.0.0"
     val kotlinCoroutinesVersion = "1.9.0-RC"
     val glideVersion = "4.16.0"
-    
+
     // androidx
     implementation("androidx.appcompat:appcompat:$appcompatVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.fragment:fragment-ktx:1.8.2")
-    
+
     // google
     implementation("com.google.android.material:material:$materialVersion")
-    
+
     // jetbrains
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinCoroutinesVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-    
+
     // dagger
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-compiler:2.51.1")
-    
+
     // Robok
     implementation(project(":robok:robok-compiler"))
     implementation(project(":robok:robok-diagnostic"))
-    
+
     // Features
     implementation(project(":feature:feature-model"))
     implementation(project(":feature:feature-component"))
@@ -97,12 +99,37 @@ dependencies {
     implementation(project(":feature:feature-res:strings"))
     implementation(project(":feature:feature-setting"))
     implementation(project(":feature:feature-terminal"))
-    
+
     implementation("com.github.aquilesTrindade.trindade-util:filepicker:3.0.9")
-    
+
     implementation("com.github.bumptech.glide:glide:$glideVersion")
     kapt("com.github.bumptech.glide:compiler:$glideVersion")
-    
+
     // Add desugaring dependency
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
+
+fun getGitHash(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine("git", "rev-parse", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
+}
+
+fun getGitBranch(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine("git", "rev-parse", "--abbrev-ref", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
+}
+
+fun getVersionName(): String {
+    val baseVersion = "1.0.0"
+    val branch = getGitBranch()
+    val hash = getGitHash()
+    return "$baseVersion-$branch-$hash"
 }
