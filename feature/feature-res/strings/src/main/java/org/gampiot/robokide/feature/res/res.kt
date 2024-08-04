c4bde4f1db7ec3102094db1ae5d690ed95cd9550
aquilesTrindade <sempedra075@gmail.com>
2024-08-03 21:43:03 -0300

    refactor: some refactors

--- feature/feature-res/strings/src/main/java/org/gampiot/robokide/feature/res/res.kt
+++ feature/feature-res/strings/src/main/java/org/gampiot/robokide/feature/res/res.kt
@@ -1,7 +1,7 @@
 package org.gampiot.robokide.feature.res
 
-typealias Layouts = org.gampiot.robokide.feature.res.R.layout
-typealias Ids = org.gampiot.robokide.feature.res.R.id
-typealias Drawables = org.gampiot.robokide.feature.res.R.drawable
-typealias Mipmaps = org.gampiot.robokide.feature.res.R.mipmap
+typealias Layouts = org.gampiot.robokide.R.layout
+typealias Ids = org.gampiot.robokide.R.id
+typealias Drawables = org.gampiot.robokide.R.drawable
+typealias Mipmaps = org.gampiot.robokide.R.mipmap
 typealias Strings = org.gampiot.robokide.feature.res.R.string
