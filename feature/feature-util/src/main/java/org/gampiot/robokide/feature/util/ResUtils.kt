93c51e79c0f49066ee69a438898773c243eba541
aquilesTrindade <sempedra075@gmail.com>
2024-08-04 01:48:36 -0300

    fix: ResUtils not a cladd

--- feature/feature-util/src/main/java/org/gampiot/robokide/feature/util/ResUtils.kt
+++ feature/feature-util/src/main/java/org/gampiot/robokide/feature/util/ResUtils.kt
@@ -5,20 +5,23 @@ import android.util.TypedValue
 import androidx.annotation.AttrRes
 import androidx.core.content.ContextCompat
 
-private lateinit var context: Context
-
-fun getAttrColor(@AttrRes resId: Int): Int {
-    val typedValue = TypedValue()
-    context.theme.resolveAttribute(resId, typedValue, true)
-    return if (typedValue.resourceId != 0) {
-        ContextCompat.getColor(context, typedValue.resourceId)
-    } else {
-        typedValue.data
-    }
-}
-
-fun setResContext(ctx: Context) {
-    context = ctx
+class ResUtils {
+ 
+     private lateinit var context: Context
+     
+     fun getAttrColor(@AttrRes resId: Int): Int {
+          val typedValue = TypedValue()
+          context.theme.resolveAttribute(resId, typedValue, true)
+          return if (typedValue.resourceId != 0) {
+              ContextCompat.getColor(context, typedValue.resourceId)
+          } else {
+              typedValue.data
+          }
+     }
+     
+     fun setResContext(ctx: Context) {
+          context = ctx
+     }
 }
 
 typealias AndroidAttr = android.R.attr
