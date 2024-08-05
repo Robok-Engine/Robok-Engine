15d12ec62965f545b2368e09f6d91b0d6fd1c0c7
aquilesTrindade <sempedra075@gmail.com>
2024-08-05 03:53:06 -0300

    fix: 'this' not defined

--- app/src/main/java/org/gampiot/robok/ui/fragments/home/Fp.kt
+++ app/src/main/java/org/gampiot/robok/ui/fragments/home/Fp.kt
@@ -2,10 +2,12 @@ package org.gampiot.robok.ui.fragments.home
 
 import android.content.Context
 
+import androidx.annotation.NonNull
+
 import dev.trindadedev.lib.filepicker.view.FilePickerDialog
 import dev.trindadedev.lib.filepicker.model.DialogProperties
 
-class Fp (private val context: Context = null, private val props: DialogProperties = DialogProperties()) : FilePickerDialog(context, props) {
+class Fp (@NonNull private val context: Context = null, private val props: DialogProperties = DialogProperties()) : FilePickerDialog(context, props) {
 
     override fun requestStoragePermission () {
          super.requestStoragePermission()
