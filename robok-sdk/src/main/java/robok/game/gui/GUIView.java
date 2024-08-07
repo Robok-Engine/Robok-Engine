ffc30eeb2fa399f95106b5b94fc73529b5564183
aquilesTrindade <sempedra075@gmail.com>
2024-08-07 00:51:17 -0300

    fix: setHeight and setWidth method without type

--- robok-sdk/src/main/java/robok/game/gui/GUIView.java
+++ robok-sdk/src/main/java/robok/game/gui/GUIView.java
@@ -51,11 +51,11 @@ public class GUIView implements GUIViewListener {
           this.id = id;
      }
      
-     public setHeight (int height) {
+     public void setHeight (int height) {
           this.height = height;
      }
      
-     public setWidth (int width) {
+     public void setWidth (int width) {
           this.width = width;
      }
      
