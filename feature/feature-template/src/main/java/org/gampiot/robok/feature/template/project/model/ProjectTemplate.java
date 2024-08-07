18f00e43b59a430765ba2367940206213febf8d8
aquilesTrindade <sempedra075@gmail.com>
2024-08-07 04:40:57 -0300

    feat, fix: add ProjectTemplate module, add dependencies to fix

---
+++ feature/feature-template/src/main/java/org/gampiot/robok/feature/template/project/model/ProjectTemplate.java
@@ -0,0 +1,39 @@
+package org.gampiot.robok.feature.template;
+
+import androidx.annotation.DrawableRes;
+
+public class ProjectTemplate {
+	
+	public String name;
+	public boolean javaSupport;
+	public boolean kotlinSupport;
+	@DrawableRes public int imageResId;
+	
+	public void setName (String name) {
+		this.name = name;
+	}
+
+	public void setJavaSupport (boolean javaSupport) {
+		this.javaSupport = javaSupport;
+	} 
+	
+	public void setKotlinSupport (boolean kotlinSupport) {
+		this.kotlinSupport = kotlinSupport;
+	}
+	
+	public void setImage (@DrawableRes int imageResId) {
+		this.imageResId = imageResId;
+	}
+	
+	public String getName () {
+		return name;
+	}
+	
+	public boolean isJavaSupport () {
+		return javaSupport;
+	}
+	
+	public boolean isKotlinSupport () {
+		return kotlinSupport;
+	}
+}
--- robok/robok-aapt2/build.gradle.kts
+++ robok/robok-aapt2/build.gradle.kts
@@ -54,8 +54,9 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 dependencies {
     implementation(fileTree("libs") { include("*.jar") })
     
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     
     implementation("com.google.code.gson:gson:2.8.7")
     
