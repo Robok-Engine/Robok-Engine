bef9c9f581242784e8b225950262b89d63290708
aquilesTrindade <sempedra075@gmail.com>
2024-08-07 17:06:07 -0300

    chore: add setters for define listeners

--- app/src/main/java/org/gampiot/robok/ui/fragments/editor/EditorFragment.kt
+++ app/src/main/java/org/gampiot/robok/ui/fragments/editor/EditorFragment.kt
@@ -165,7 +165,8 @@ class EditorFragment(private val transitionAxis: Int = MaterialSharedAxis.X) : R
                  binding.diagnosticStatusImage.visibility = View.VISIBLE
             }
             override fun onDiagnosticReceive(line: Int, positionStart: Int, postionEnd: Int, msg: String) {
-                
+                addDiagnosticInEditor(positionStart, positionEnd, DiagnosticRegion.SEVERITY_ERROR, msg);
+                onDiagnosticStatusReceive(true)
             }
         }
         
