5454dd1ba251f2e92889a6dd2be1eea48ab9e91b
ThDev-only <rthiarley@gmail.com>
2024-08-07 19:05:25 -0300

    Fix: NewColorScheme

--- feature/feature-component/src/main/java/org/gampiot/robok/feature/component/editor/schemes/NewSchemeRobok.kt
+++ feature/feature-component/src/main/java/org/gampiot/robok/feature/component/editor/schemes/NewSchemeRobok.kt
@@ -1,69 +1,57 @@
 package org.gampiot.robok.feature.component.editor.schemes
 
-/*
- * Custom theme class for the Sora Editor based on SchemeDarcula.
- * This class overrides the default color scheme with custom colors.
- */
-
 import android.content.Context
 import android.util.TypedValue
 import androidx.annotation.AttrRes
 import androidx.core.content.ContextCompat
-import io.github.rosemoe.sora.widget.schemes.SchemeDarcula
 import io.github.rosemoe.sora.widget.schemes.EditorColorScheme
-import org.gampiot.robok.feature.component.R
 
-class NewSchemeRobok(context: Context) : SchemeDarcula() {
-
-    private val context: Context
+class NewSchemeRobok(context: Context) : EditorColorScheme(isDark = false) {
 
     init {
-        this.context = context
-        
-        // General editor colors
-        setColor(EditorColorScheme.WHOLE_BACKGROUND, getAttrColor(android.R.attr.colorBackground)) // Editor's background color
-        setColor(EditorColorScheme.CURRENT_LINE, getColor(R.color.scheme_robok_current_line)) // Highlight color for the current line
-        setColor(EditorColorScheme.LINE_NUMBER_PANEL, getColor(R.color.scheme_robok_line_number_panel)) // Background color for line number panel
-        setColor(EditorColorScheme.LINE_NUMBER_BACKGROUND, getColor(R.color.scheme_robok_line_number_background)) // Line number background
-        setColor(EditorColorScheme.LINE_NUMBER, getColor(R.color.scheme_robok_line_number)) // Text color for line numbers
-        
-        // Syntax highlight colors
-        setColor(EditorColorScheme.KEYWORD, getAttrColor(com.google.android.material.R.attr.colorPrimary)) // Keyword color
-        setColor(EditorColorScheme.TYPE_NAME, getColor(R.color.scheme_robok_type_name)) // Type name color (e.g., classes, interfaces)
-        setColor(EditorColorScheme.LITERAL, getColor(R.color.scheme_robok_literal)) // Literal color (e.g., numbers)
-        setColor(EditorColorScheme.STRING, getColor(R.color.scheme_robok_string)) // String literal color
-        setColor(EditorColorScheme.COMMENT, getColor(R.color.scheme_robok_comment)) // Comment color
-        setColor(EditorColorScheme.DOCS, getColor(R.color.scheme_robok_docs)) // Documentation comments color
-        setColor(EditorColorScheme.IDENTIFIER_NAME, getColor(R.color.scheme_robok_identifier)) // Variable and identifier names
-        setColor(EditorColorScheme.FUNCTION_NAME, getAttrColor(com.google.android.material.R.attr.colorOnSurface)) // Function name color
-        setColor(EditorColorScheme.OPERATOR, getColor(R.color.scheme_robok_operator)) // Operator color (e.g., +, -, *, /)
-        setColor(EditorColorScheme.ANNOTATION, getColor(R.color.scheme_robok_annotation)) // Annotation color (e.g., @Override)
-
-        // Bracket matching colors
-        setColor(EditorColorScheme.MATCHED_BRACE, getColor(R.color.scheme_robok_matched_brace)) // Matched brace highlight color
-        setColor(EditorColorScheme.UNMATCHED_BRACE, getColor(R.color.scheme_robok_unmatched_brace)) // Unmatched brace highlight color
-
-        // Selection and handles
-        setColor(EditorColorScheme.SELECTION_BACKGROUND, getColor(R.color.scheme_robok_selection_background)) // Selection background color
-        setColor(EditorColorScheme.SELECTION_HANDLE, getColor(R.color.scheme_robok_selection_handle)) // Selection handle color
-
-        // Error and warning underlines
-        setColor(EditorColorScheme.ERROR_UNDERLINE, getColor(R.color.scheme_robok_error_underline)) // Error underline color
-        setColor(EditorColorScheme.WARNING_UNDERLINE, getColor(R.color.scheme_robok_warning_underline)) // Warning underline color
-
-        // Editor guide lines
-        setColor(EditorColorScheme.GUTTER_DIVIDER, getColor(R.color.scheme_robok_gutter_divider)) // Divider line in the gutter
-        setColor(EditorColorScheme.NON_PRINTABLE_CHAR, getColor(R.color.scheme_robok_non_printable)) // Non-printable character color
-        setColor(EditorColorScheme.CARET, getColor(R.color.scheme_robok_caret)) // Caret (cursor) color
-        setColor(EditorColorScheme.CARET_LINE, getColor(R.color.scheme_robok_caret_line)) // Caret line color
-
-        // Additional UI components
-        setColor(EditorColorScheme.BLOCK_LINE, getColor(R.color.scheme_robok_block_line)) // Block line color
-        setColor(EditorColorScheme.BLOCK_LINE_CURRENT, getColor(R.color.scheme_robok_block_line_current)) // Current block line color
-        setColor(EditorColorScheme.RIGHT_MARGIN, getColor(R.color.scheme_robok_right_margin)) // Right margin line color
-        setColor(EditorColorScheme.CODE_LENS, getColor(R.color.scheme_robok_code_lens)) // Code lens color
-        setColor(EditorColorScheme.SIGNATURE, getColor(R.color.scheme_robok_signature)) // Signature color
-        setColor(EditorColorScheme.LINK, getColor(R.color.scheme_robok_link)) // Link color in text
+        setColor(EditorColorScheme.WHOLE_BACKGROUND, getAttrColor(android.R.attr.colorBackground))
+        setColor(EditorColorScheme.CURRENT_LINE, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_current_line))
+        setColor(EditorColorScheme.LINE_NUMBER_PANEL, getAttrColor(android.R.attr.colorBackground))
+        setColor(EditorColorScheme.LINE_NUMBER_BACKGROUND, getAttrColor(android.R.attr.colorBackground))
+        setColor(EditorColorScheme.KEYWORD, getAttrColor(com.google.android.material.R.attr.colorPrimary))
+        setColor(EditorColorScheme.FUNCTION_NAME, getAttrColor(com.google.android.material.R.attr.colorOnSurface))
+        setColor(EditorColorScheme.ATTRIBUTE_VALUE, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_attribute_value))
+        setColor(EditorColorScheme.ATTRIBUTE_NAME, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_attribute_name))
+        setColor(EditorColorScheme.HTML_TAG, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_html_tag))
+        setColor(EditorColorScheme.ANNOTATION, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_annotation))
+        setColor(EditorColorScheme.IDENTIFIER_NAME, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_identifier_name))
+        setColor(EditorColorScheme.IDENTIFIER_VAR, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_identifier_var))
+        setColor(EditorColorScheme.LITERAL, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_literal))
+        setColor(EditorColorScheme.OPERATOR, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_operator))
+        setColor(EditorColorScheme.COMMENT, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_comment))
+        setColor(EditorColorScheme.TEXT_SELECTED, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_text_selected))
+        setColor(EditorColorScheme.MATCHED_TEXT_BACKGROUND, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_matched_text_background))
+        setColor(EditorColorScheme.SELECTION_HANDLE, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_selection_handle))
+        setColor(EditorColorScheme.SELECTION_INSERT, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_selection_insert))
+        setColor(EditorColorScheme.SELECTED_TEXT_BACKGROUND, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_selected_text_background))
+        setColor(EditorColorScheme.UNDERLINE, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_underline))
+        setColor(EditorColorScheme.SIDE_BLOCK_LINE, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_side_block_line))
+        setColor(EditorColorScheme.NON_PRINTABLE_CHAR, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_non_printable_char))
+        setColor(EditorColorScheme.STICKY_SCROLL_DIVIDER, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_sticky_scroll_divider))
+        setColor(EditorColorScheme.DIAGNOSTIC_TOOLTIP_ACTION, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_diagnostic_tooltip_action))
+        setColor(EditorColorScheme.DIAGNOSTIC_TOOLTIP_DETAILED_MSG, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_diagnostic_tooltip_detailed_msg))
+        setColor(EditorColorScheme.DIAGNOSTIC_TOOLTIP_BRIEF_MSG, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_diagnostic_tooltip_brief_msg))
+        setColor(EditorColorScheme.DIAGNOSTIC_TOOLTIP_BACKGROUND, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_diagnostic_tooltip_background))
+        setColor(EditorColorScheme.FUNCTION_CHAR_BACKGROUND_STROKE, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_function_char_background_stroke))
+        setColor(EditorColorScheme.HARD_WRAP_MARKER, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_hard_wrap_marker))
+        setColor(EditorColorScheme.TEXT_INLAY_HINT_FOREGROUND, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_text_inlay_hint_foreground))
+        setColor(EditorColorScheme.TEXT_INLAY_HINT_BACKGROUND, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_text_inlay_hint_background))
+        setColor(EditorColorScheme.SNIPPET_BACKGROUND_EDITING, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_snippet_background_editing))
+        setColor(EditorColorScheme.SNIPPET_BACKGROUND_RELATED, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_snippet_background_related))
+        setColor(EditorColorScheme.SNIPPET_BACKGROUND_INACTIVE, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_snippet_background_inactive))
+        setColor(EditorColorScheme.SIGNATURE_TEXT_NORMAL, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_signature_text_normal))
+        setColor(EditorColorScheme.SIGNATURE_TEXT_HIGHLIGHTED_PARAMETER, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_signature_text_highlighted_parameter))
+        setColor(EditorColorScheme.STATIC_SPAN_BACKGROUND, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_static_span_background))
+        setColor(EditorColorScheme.STATIC_SPAN_FOREGROUND, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_static_span_foreground))
+        setColor(EditorColorScheme.SIGNATURE_BACKGROUND, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_signature_background))
+        setColor(EditorColorScheme.LINE_NUMBER, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_line_number))
+        setColor(EditorColorScheme.LINE_NUMBER_CURRENT, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_line_number_current))
+        setColor(EditorColorScheme.LINE_DIVIDER, getColor(org.gampiot.robok.feature.component.R.color.scheme_robok_line_divider))
     }
 
     private fun getAttrColor(@AttrRes resId: Int): Int {
@@ -76,7 +64,7 @@ class NewSchemeRobok(context: Context) : SchemeDarcula() {
         }
     }
 
-    private fun getColor(resId: Int): Int {
-        return ContextCompat.getColor(context, resId)
+    private fun getColor(colorResId: Int): Int {
+        return ContextCompat.getColor(context, colorResId)
     }
 }
