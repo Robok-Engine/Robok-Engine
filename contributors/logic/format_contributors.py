1f9e6de315794d15e78124113c4900db20c1cb97
aquilesTrindade <sempedra075@gmail.com>
2024-08-07 23:07:54 -0300

    fix: role attribute is removed in PR's

--- contributors/logic/format_contributors.py
+++ contributors/logic/format_contributors.py
@@ -6,12 +6,13 @@ def format_contributors(input_file, output_file):
         contributors = json.load(infile)
 
     with open(output_file, 'w') as outfile:
-        outfile.write('| Contributor | Contributions |\n')
-        outfile.write('| ------------ | -------------- |\n')
+        outfile.write('| Contributor | Contributions | Role |\n')
+        outfile.write('| ------------ | -------------- | ---- |\n')
         for contributor in contributors:
             name = contributor['login']
             contributions = contributor['contributions']
-            outfile.write(f'| {name} | {contributions} |\n')
+            role = contributor.get('role', 'N/A') 
+            outfile.write(f'| {name} | {contributions} | {role} |\n')
 
 if __name__ == "__main__":
     format_contributors('contributors/contributors_github.json', 'contributors/TABLE_CONTRIBUTORS_GITHUB.md')
