16fead32818662206c48dcfacf58f13125636f00
aquilesTrindade <sempedra075@gmail.com>
2024-08-10 14:46:13 -0300

    fix: ',' expected

--- feature/feature-template/src/main/java/org/gampiot/robok/feature/template/project/adapter/ProjectTemplateAdapter.java
+++ feature/feature-template/src/main/java/org/gampiot/robok/feature/template/project/adapter/ProjectTemplateAdapter.java
@@ -24,7 +24,7 @@ public class ProjectTemplateAdapter extends RecyclerView.Adapter<ProjectTemplate
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_project_template parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_project_template, parent, false);
         return new ViewHolder(view);
     }
 
