bb0d13db740b7ea04454555eefefa2284b6d908f
aquilesTrindade <sempedra075@gmail.com>
2024-08-11 00:38:29 -0300

    fix: Toast import

--- app/src/main/java/org/gampiot/robok/ui/fragments/project/template/adapter/ProjectTemplateAdapter.java
+++ app/src/main/java/org/gampiot/robok/ui/fragments/project/template/adapter/ProjectTemplateAdapter.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
