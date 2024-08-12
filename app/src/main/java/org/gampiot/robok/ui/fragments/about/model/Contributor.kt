373b24dc8ab11fce1495fa4bdb0e58001218d9f6
aquilesTrindade <sempedra075@gmail.com>
2024-08-11 22:54:00 -0300

    fix: add all keys on Contributor

--- app/src/main/java/org/gampiot/robok/ui/fragments/about/model/Contributor.kt
+++ app/src/main/java/org/gampiot/robok/ui/fragments/about/model/Contributor.kt
@@ -4,8 +4,24 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Contributor(
-    val avatar_url: String,
     val login: String,
-    val role: String,
-    val html_url: String
+    val id: Int,
+    val node_id: String,
+    val avatar_url: String,
+    val gravatar_id: String,
+    val url: String,
+    val html_url: String,
+    val followers_url: String,
+    val following_url: String,
+    val gists_url: String,
+    val starred_url: String,
+    val subscriptions_url: String,
+    val organizations_url: String,
+    val repos_url: String,
+    val events_url: String,
+    val received_events_url: String,
+    val type: String,
+    val site_admin: Boolean,
+    val contributions: Int,
+    val role: String
 )
