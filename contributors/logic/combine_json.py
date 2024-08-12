47d49869e8f71833201e3a9b17260d228c874de8
aquilesTrindade <sempedra075@gmail.com>
2024-08-11 23:42:16 -0300

    workflow: test fix re-creation

---
+++ contributors/logic/combine_json.py
@@ -0,0 +1,28 @@
+import json
+
+# load data from the API.
+with open('contributors/contributors_github_temp.json', 'r') as f:
+    github_data = json.load(f)
+
+# load existing data
+with open('contributors/contributors_github.json', 'r') as f:
+    existing_data = json.load(f)
+
+# convert existing data to a dictionary based on login and id
+existing_dict = {(user['login'], user['id']): user for user in existing_data}
+
+# update or add API data
+for user in github_data:
+    key = (user['login'], user['id'])
+    if key in existing_dict:
+        existing_dict[key].update(user)
+    else:
+        existing_dict[key] = user
+
+# convert back to a list
+combined_data = list(existing_dict.values())
+
+# save the combined json
+with open('contributors/contributors_combined.json', 'w') as f:
+    json.dump(combined_data, f, indent=2)
+    
