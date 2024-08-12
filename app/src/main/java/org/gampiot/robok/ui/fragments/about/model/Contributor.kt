a2428c382fda6ea74017168a4e09872aec40036e
aquilesTrindade <sempedra075@gmail.com>
2024-08-11 22:30:49 -0300

    fix: add @Serializable  in model

--- app/src/main/java/org/gampiot/robok/ui/fragments/about/model/Contributor.kt
+++ app/src/main/java/org/gampiot/robok/ui/fragments/about/model/Contributor.kt
@@ -1,5 +1,8 @@
 package org.gampiot.robok.ui.fragments.about.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Contributor(
     val avatar_url: String,
     val login: String,
