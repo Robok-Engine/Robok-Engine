ed87b27c192f67719cd59c04912fd840aa48598c
devsuay <devsuay@gmail.com>
2024-08-15 20:41:03 -0300

    fix: '{' expected

--- feature/feature-treeview/src/main/java/org/gampiot/robok/feature/treeview/file/example/FileTreeNodeViewHolder.java
+++ feature/feature-treeview/src/main/java/org/gampiot/robok/feature/treeview/file/example/FileTreeNodeViewHolder.java
@@ -20,38 +20,38 @@ public class FileTreeNodeViewHolder extends TreeNode.BaseNodeViewHolder<FileNode
 
     @Override
     public View createNodeView(TreeNode node, FileNode value) {
-        final LayoutInflater inflater = LayoutInflater.from(context);
-        final View view = inflater.inflate(R.layout.tree_node_item, null, false);
+        if (node != null || value != null) {
+            final LayoutInflater inflater = LayoutInflater.from(context);
+            final View view = inflater.inflate(R.layout.tree_node_item, null, false);
 
-        LinearLayout layout = view.findViewById(R.id.layout);
-        TextView textView = view.findViewById(R.id.path);
-        textView.setText(value.name);
+            LinearLayout layout = view.findViewById(R.id.layout);
+            TextView textView = view.findViewById(R.id.path);
+            textView.setText(value.name);
+            ImageView iconView = view.findViewById(R.id.icon);
+            if (value.isDirectory) {
+                iconView.setImageResource(R.drawable.ic_folder);
+            } else {
+                iconView.setImageResource(R.drawable.ic_file);
+            }
 
-        ImageView iconView = view.findViewById(R.id.icon);
-        if (value.isDirectory) {
-            iconView.setImageResource(R.drawable.ic_folder);
-        } else {
-            iconView.setImageResource(R.drawable.ic_file);
+            ImageView expandCollapseIcon = view.findViewById(R.id.expandCollapse);
+            if (node.isLeaf()) {
+                expandCollapseIcon.setVisibility(View.GONE);
+            } else {
+                expandCollapseIcon.setVisibility(View.VISIBLE);
+                expandCollapseIcon.setImageResource(node.isExpanded() ? R.drawable.ic_collapse : R.drawable.ic_expand);
+                expandCollapseIcon.setOnClickListener(v -> {
+                    if (node != null) {
+                         if (node.isExpanded()) {
+                              treeView.collapseNode(node);
+                         } else {
+                              treeView.expandNode(node);
+                         }
+                         updateExpandCollapseIcon(expandCollapseIcon, node.isExpanded());
+                    }
+                });
+            }
         }
-
-        ImageView expandCollapseIcon = view.findViewById(R.id.expandCollapse);
-        if (node.isLeaf()) {
-            expandCollapseIcon.setVisibility(View.GONE);
-        } else {
-            expandCollapseIcon.setVisibility(View.VISIBLE);
-            expandCollapseIcon.setImageResource(node.isExpanded() ? R.drawable.ic_collapse : R.drawable.ic_expand);
-            expandCollapseIcon.setOnClickListener(v -> {
-                if (node != null) {
-                     if (node.isExpanded()) {
-                          treeView.collapseNode(node);
-                     } else {
-                          treeView.expandNode(node);
-                     }
-                     updateExpandCollapseIcon(expandCollapseIcon, node.isExpanded());
-                }
-            });
-        }
-
         return view;
     }
 
