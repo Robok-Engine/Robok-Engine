ab0a74c7c463e1db288e4c1885d9342160e13ac7
devsuay <devsuay@gmail.com>
2024-08-15 19:46:46 -0300

    fix: treeView not found

---
+++ feature/feature-treeview/src/main/java/org/gampiot/robok/feature/treeview/file/example/FileNode.java
@@ -0,0 +1,11 @@
+package org.gampiot.robok.feature.treeview.file.example;
+
+public class FileNode {
+      String name;
+      boolean isDirectory;
+      
+      public FileNode(String name, boolean isDirectory) {
+           this.name = name;
+           this.isDirectory = isDirectory;
+      }
+}
