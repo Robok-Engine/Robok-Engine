56f83b3b2846828363591f2eb1441dbed0e10518
ThDev-only <174269512+ThDev-only@users.noreply.github.com>
2024-08-17 21:17:59 +0000

    Build: prepare for v1.0.0 (2) release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "org.gampiot.robok"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = getVersionName()
 
         // git fields
