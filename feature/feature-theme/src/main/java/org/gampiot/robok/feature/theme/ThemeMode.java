d3cf444bf9e02224a71157d1cc8d2dae1f00cbc7
devsuay <devsuay@gmail.com>
2024-08-16 22:59:26 -0300

    fix: add missing icon, add missing ThemeMode class

---
+++ feature/feature-theme/src/main/java/org/gampiot/robok/feature/theme/ThemeMode.java
@@ -0,0 +1,7 @@
+package org.gampiot.robok.feature.theme;
+
+public class ThemeMode {
+     public static final int AUTO = 0;
+     public static final int LIGHT = 1;
+     public static final int DARK = 2;
+}
