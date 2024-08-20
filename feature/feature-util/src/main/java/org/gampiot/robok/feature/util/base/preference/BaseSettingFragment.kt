989516d80f7f5170a0cf7bde53c819b30907ba5e
aquilesTrindade <sempedra075@gmail.com>
2024-08-20 13:10:34 -0300

    fix: IdRes not found

--- feature/feature-util/src/main/java/org/gampiot/robok/feature/util/base/preference/BaseSettingFragment.kt
+++ feature/feature-util/src/main/java/org/gampiot/robok/feature/util/base/preference/BaseSettingFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+import androidx.annotation.IdRes
+
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.appbar.MaterialToolbar
