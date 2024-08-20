690abb48405229cc9bb8c6dcbdc7f0ac8991e671
aquilesTrindade <sempedra075@gmail.com>
2024-08-20 14:26:53 -0300

    fix: no id passed

--- app/src/main/java/org/gampiot/robok/ui/activities/MainActivity.kt
+++ app/src/main/java/org/gampiot/robok/ui/activities/MainActivity.kt
@@ -11,9 +11,7 @@ import org.gampiot.robok.R
 import org.gampiot.robok.ui.fragments.home.HomeFragment
 import org.gampiot.robok.feature.util.base.RobokActivity
 
-class MainActivity(
-    @IdRes private val fragmentLayoutResId: Int
-) : RobokActivity(fragmentLayoutResId) {
+class MainActivity() : RobokActivity(R.id.fragment_container) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
