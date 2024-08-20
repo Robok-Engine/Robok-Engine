c2fd49a5c2d323c70c5a4dc546ffb02dbb8c5432
ThDev-only <rthiarley@gmail.com>
2024-08-20 12:23:33 -0300

    fix aapt2 compiler

--- feature/feature-util/src/main/java/org/gampiot/robok/feature/util/application/RobokApp.java
+++ feature/feature-util/src/main/java/org/gampiot/robok/feature/util/application/RobokApp.java
@@ -43,7 +43,7 @@ public class RobokApp extends Application {
     public void onCreate() {
          super.onCreate();
          instance = this;
-         robokContext = this;
+         robokContext = getApplicationContext();
          Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
               Intent intent = new Intent(getApplicationContext(), DebugActivity.class);
               intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
