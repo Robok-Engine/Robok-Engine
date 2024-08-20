71339cd802d65e730a83b9d8d1a4eddd76ad711f
aquilesTrindade <sempedra075@gmail.com>
2024-08-19 23:47:49 -0300

    some changes

--- feature/feature-util/src/main/java/org/gampiot/robok/feature/util/Utils.kt
+++ feature/feature-util/src/main/java/org/gampiot/robok/feature/util/Utils.kt
@@ -1,65 +1,92 @@
 package org.gampiot.robok.feature.util
 
 import android.view.View
+
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.children
-import androidx.core.graphics.Insets
 
-fun View.enableEdgeToEdgePaddingListener(ime: Boolean = false, top: Boolean = false,
-                                             extra: ((Insets) -> Unit)? = null) {
-        if (fitsSystemWindows) throw IllegalArgumentException("must have fitsSystemWindows disabled")
-        if (this is AppBarLayout) {
-            if (ime) throw IllegalArgumentException("AppBarLayout must have ime flag disabled")
-            val collapsingToolbarLayout = children.find { it is CollapsingToolbarLayout } as CollapsingToolbarLayout?
-            collapsingToolbarLayout?.let {
-                ViewCompat.setOnApplyWindowInsetsListener(it) { _, insets -> insets }
-            }
-            val expandedTitleMarginStart = collapsingToolbarLayout?.expandedTitleMarginStart
-            val expandedTitleMarginEnd = collapsingToolbarLayout?.expandedTitleMarginEnd
-            ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
-                val cutoutAndBars = insets.getInsets(
-                    WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
-                )
-                (v as AppBarLayout).children.forEach {
-                    if (it is CollapsingToolbarLayout) {
-                        val es = expandedTitleMarginStart!! + if (it.layoutDirection
-                            == View.LAYOUT_DIRECTION_LTR) cutoutAndBars.left else cutoutAndBars.right
-                        if (es != it.expandedTitleMarginStart) it.expandedTitleMarginStart = es
-                        val ee = expandedTitleMarginEnd!! + if (it.layoutDirection
-                            == View.LAYOUT_DIRECTION_RTL) cutoutAndBars.left else cutoutAndBars.right
-                        if (ee != it.expandedTitleMarginEnd) it.expandedTitleMarginEnd = ee
-                    }
-                    it.setPadding(cutoutAndBars.left, 0, cutoutAndBars.right, 0)
-                }
-                v.setPadding(0, cutoutAndBars.top, 0, 0)
-                val i = insets.getInsetsIgnoringVisibility(WindowInsetsCompat.Type.systemBars()
-                        or WindowInsetsCompat.Type.displayCutout())
-                extra?.invoke(cutoutAndBars)
-                return@setOnApplyWindowInsetsListener WindowInsetsCompat.Builder(insets)
-                    .setInsets(WindowInsetsCompat.Type.systemBars()
-                            or WindowInsetsCompat.Type.displayCutout(), Insets.of(cutoutAndBars.left, 0, cutoutAndBars.right, cutoutAndBars.bottom))
-                    .setInsetsIgnoringVisibility(WindowInsetsCompat.Type.systemBars()
-                            or WindowInsetsCompat.Type.displayCutout(), Insets.of(i.left, 0, i.right, i.bottom))
-                    .build()
-            }
-        } else {
-            val pl = paddingLeft
-            val pt = paddingTop
-            val pr = paddingRight
-            val pb = paddingBottom
-            ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
-                val mask = WindowInsetsCompat.Type.systemBars() or
-                        WindowInsetsCompat.Type.displayCutout() or
-                        if (ime) WindowInsetsCompat.Type.ime() else 0
-                val i = insets.getInsets(mask)
-                v.setPadding(pl + i.left, pt + (if (top) i.top else 0), pr + i.right,
-                    pb + i.bottom)
-                extra?.invoke(i)
-                return@setOnApplyWindowInsetsListener WindowInsetsCompat.Builder(insets)
-                    .setInsets(mask, Insets.NONE)
-                    .setInsetsIgnoringVisibility(mask, Insets.NONE)
-                    .build()
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
+
+fun View.enableEdgeToEdgePaddingListener(
+    ime: Boolean = false, 
+    top: Boolean = false,
+    extra: ((Insets) -> Unit)? = null
+) {
+    if (fitsSystemWindows) throw IllegalArgumentException("fitsSystemWindows must be disabled")
+    
+    if (this is AppBarLayout) {
+        if (ime) throw IllegalArgumentException("AppBarLayout cannot have ime flag enabled")
+
+        val collapsingToolbarLayout = children.find { it is CollapsingToolbarLayout } as? CollapsingToolbarLayout
+
+        collapsingToolbarLayout?.let {
+            ViewCompat.setOnApplyWindowInsetsListener(it) { _, insets ->
+                insets
             }
         }
+
+        val expandedTitleMarginStart = collapsingToolbarLayout?.expandedTitleMarginStart
+        val expandedTitleMarginEnd = collapsingToolbarLayout?.expandedTitleMarginEnd
+
+        ViewCompat.setOnApplyWindowInsetsListener(this) { view, insets ->
+            val cutoutAndBars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+            )
+
+            (view as AppBarLayout).children.forEach { child ->
+                if (child is CollapsingToolbarLayout) {
+                    val leftInset = if (child.layoutDirection == View.LAYOUT_DIRECTION_LTR) cutoutAndBars.left else cutoutAndBars.right
+                    val rightInset = if (child.layoutDirection == View.LAYOUT_DIRECTION_RTL) cutoutAndBars.left else cutoutAndBars.right
+
+                    if (expandedTitleMarginStart != null && expandedTitleMarginStart != (child.expandedTitleMarginStart + leftInset)) {
+                        child.expandedTitleMarginStart = expandedTitleMarginStart + leftInset
+                    }
+                    if (expandedTitleMarginEnd != null && expandedTitleMarginEnd != (child.expandedTitleMarginEnd + rightInset)) {
+                        child.expandedTitleMarginEnd = expandedTitleMarginEnd + rightInset
+                    }
+                }
+                child.setPadding(cutoutAndBars.left, 0, cutoutAndBars.right, 0)
+            }
+            view.setPadding(0, cutoutAndBars.top, 0, 0)
+
+            val insetsIgnoringVisibility = insets.getInsetsIgnoringVisibility(
+                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+            )
+
+            extra?.invoke(cutoutAndBars)
+            WindowInsetsCompat.Builder(insets)
+                .setInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout(), 
+                    Insets.of(cutoutAndBars.left, 0, cutoutAndBars.right, cutoutAndBars.bottom))
+                .setInsetsIgnoringVisibility(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout(), 
+                    Insets.of(insetsIgnoringVisibility.left, 0, insetsIgnoringVisibility.right, insetsIgnoringVisibility.bottom))
+                .build()
+        }
+    } else {
+        val initialPaddingLeft = paddingLeft
+        val initialPaddingTop = paddingTop
+        val initialPaddingRight = paddingRight
+        val initialPaddingBottom = paddingBottom
+
+        ViewCompat.setOnApplyWindowInsetsListener(this) { view, insets ->
+            val mask = WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout() or 
+                if (ime) WindowInsetsCompat.Type.ime() else 0
+
+            val insetsValue = insets.getInsets(mask)
+            view.setPadding(
+                initialPaddingLeft + insetsValue.left, 
+                initialPaddingTop + (if (top) insetsValue.top else 0), 
+                initialPaddingRight + insetsValue.right, 
+                initialPaddingBottom + insetsValue.bottom
+            )
+
+            extra?.invoke(insetsValue)
+            WindowInsetsCompat.Builder(insets)
+                .setInsets(mask, Insets.NONE)
+                .setInsetsIgnoringVisibility(mask, Insets.NONE)
+                .build()
+        }
     }
+}
