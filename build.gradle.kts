dad4c90ddc489c34939073412e449f08dc70bae6
trindadedev13 <devsuay@gmail.com>
2024-08-31 14:52:56 -0300

    refactor: Delete settings, move about to home, add compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("com.android.library") version "8.6.0" apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("com.android.library") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
     id("com.mikepenz.aboutlibraries.plugin") version "11.2.3" apply false
 }
