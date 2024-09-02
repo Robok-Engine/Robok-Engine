c10d8ab9c0a03a48cc794cdcd9c7846d1d591bac
trindadedev13 <devsuay@gmail.com>
2024-09-02 19:48:44 -0300

    fixes

--- feature/feature-editor/src/main/java/org/gampiot/robok/feature/editor/Manager.kt
+++ feature/feature-editor/src/main/java/org/gampiot/robok/feature/editor/Manager.kt
@@ -1,9 +1,10 @@
 package org.gampiot.robok.feature.editor
 
-// form A.I lol
+// From I.A
 
-import kotlinx.coroutines.flow.StateFlow
-
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import org.gampiot.robok.feature.settings.viewmodels.AppPreferencesViewModel
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -12,12 +13,18 @@ class Manager : KoinComponent {
 
     private val appPreferencesViewModel: AppPreferencesViewModel by inject()
 
-    val editorTheme: StateFlow<Int> get() = appPreferencesViewModel.editorTheme
+    // Expose the Flow from the ViewModel
+    val editorTheme: Flow<Int> get() = appPreferencesViewModel.editorTheme
 
+    // Method to get the current theme value
     fun getEditorTheme(): Int {
-        return editorTheme.value
+        return runBlocking {
+            // Collect the first value from the Flow
+            editorTheme.first()
+        }
     }
 
+    // Method to set a new theme value
     fun setEditorTheme(value: Int) {
         appPreferencesViewModel.changeEditorTheme(value)
     }
