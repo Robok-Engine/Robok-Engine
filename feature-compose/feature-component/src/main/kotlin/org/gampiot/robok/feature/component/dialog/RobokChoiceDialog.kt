ddafd994e7bb985938ef4fc099200626dd3bad8a
trindadedev13 <devsuay@gmail.com>
2024-09-02 21:29:10 -0300

    fixes

--- feature-compose/feature-component/src/main/kotlin/org/gampiot/robok/feature/component/dialog/RobokChoiceDialog.kt
+++ feature-compose/feature-component/src/main/kotlin/org/gampiot/robok/feature/component/dialog/RobokChoiceDialog.kt
@@ -12,6 +12,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
 
 import org.gampiot.robok.feature.res.Strings
 import org.gampiot.robok.feature.component.radio.EnumRadioController
