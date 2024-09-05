e31d40ffba657e99e00f0663f1d54f09909fed75
trindadedev13 <devsuay@gmail.com>
2024-09-04 22:05:13 -0300

    chore: move vegabobo/PreferenceItem to /PreferenceItem

--- feature-compose/feature-component/src/main/kotlin/org/gampiot/robok/feature/component/preferences/PreferenceItem.kt
+++ feature-compose/feature-component/src/main/kotlin/org/gampiot/robok/feature/component/preferences/PreferenceItem.kt
@@ -1,4 +1,4 @@
-package org.gampiot.robok.feature.component.preferences.vegabobo
+package org.gampiot.robok.feature.component.preferences
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
--- feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/screens/editor/SettingsCodeEditorScreen.kt
+++ feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/screens/editor/SettingsCodeEditorScreen.kt
@@ -15,7 +15,7 @@ import org.koin.androidx.compose.koinViewModel
 import org.gampiot.robok.feature.component.ApplicationScreen
 import org.gampiot.robok.feature.component.appbars.TopBar
 import org.gampiot.robok.feature.component.Title
-import org.gampiot.robok.feature.component.preferences.vegabobo.PreferenceItem
+import org.gampiot.robok.feature.component.preferences.PreferenceItem
 import org.gampiot.robok.feature.component.preferences.bunny.PreferenceItemChoice
 import org.gampiot.robok.feature.settings.viewmodels.AppPreferencesViewModel
 import org.gampiot.robok.feature.res.Strings
