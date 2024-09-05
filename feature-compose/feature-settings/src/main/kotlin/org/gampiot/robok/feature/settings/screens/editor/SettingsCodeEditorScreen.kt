1b4ed6b95ae1cffd05b866aa9412d849b6fb6c52
trindadedev13 <devsuay@gmail.com>
2024-09-04 21:20:42 -0300

    fix: LocalContext not found

--- feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/screens/editor/SettingsCodeEditorScreen.kt
+++ feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/screens/editor/SettingsCodeEditorScreen.kt
@@ -1,5 +1,6 @@
 package org.gampiot.robok.feature.settings.screens.editor
 
+import android.content.Context
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -7,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.compose.ui.platform.LocalContext
 
 import org.koin.androidx.compose.koinViewModel
 
