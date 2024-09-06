b2976ad2aebb71148022f755c06848f4e652c9cb
trindadedev13 <devsuay@gmail.com>
2024-09-06 19:52:31 -0300

    feat: a

--- feature/feature-editor/src/main/java/org/gampiot/robok/feature/editor/languages/java/store/models/ClassItem.kt
+++ feature/feature-editor/src/main/java/org/gampiot/robok/feature/editor/languages/java/store/models/ClassItem.kt
@@ -1,8 +1,5 @@
 package org.gampiot.robok.feature.editor.languages.java.store.models
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class ClassItem(
     val className String,
     val classPackageName: String
