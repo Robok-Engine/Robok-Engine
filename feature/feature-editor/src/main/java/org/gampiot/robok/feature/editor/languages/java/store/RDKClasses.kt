a6bde5bb41479b2bcff8d0ade4cafe315ff62c86
trindadedev13 <devsuay@gmail.com>
2024-09-06 21:47:06 -0300

    a

--- feature/feature-editor/src/main/java/org/gampiot/robok/feature/editor/languages/java/store/RDKClasses.kt
+++ feature/feature-editor/src/main/java/org/gampiot/robok/feature/editor/languages/java/store/RDKClasses.kt
@@ -1,25 +1,15 @@
 package org.gampiot.robok.feature.editor.languages.java.store
 
-/*
-* Class to get Classes from https://raw.githubusercontent.com/robok-inc/Robok-SDK/dev/versions/$RDK_VERSION/classes.json
-* And add in HashMap to use on Auto Complete
-* @author Aquiles Trindade
-*/
-
 import android.util.Log
+
+import kotlinx.coroutines.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
-import kotlinx.coroutines.runBlocking
-
 import org.gampiot.robok.feature.editor.languages.java.store.models.ClassItem
-
-import java.util.HashMap
 
 class RDKClasses {
 
@@ -28,7 +18,7 @@ class RDKClasses {
         private const val RDK_VERSION = "RDK-1"
         private const val URL = "https://raw.githubusercontent.com/robok-inc/Robok-SDK/dev/versions/$RDK_VERSION/classes.json"
     }
-    
+
     private val client = OkHttpClient()
 
     private suspend fun fetchClasses(): List<ClassItem> = withContext(Dispatchers.IO) {
@@ -55,17 +45,15 @@ class RDKClasses {
 
     suspend fun getClasses(): HashMap<String, String> {
         val classes = fetchClasses()
-        val classMap = HashMap<String, String>()
-        classes.forEach { clazz ->
-            classMap[clazz.className] = clazz.classPackageName
-        }
-        return classMap
+        return classes.associate { it.className to it.classPackageName } as HashMap<String, String>
     }
 }
 
 object RDKClassesHelper {
     @JvmStatic
-    fun getClasses(): HashMap<String, String> = runBlocking {
-        RDKClasses().getClasses()
+    fun getClasses(): HashMap<String, String> {
+        return runBlocking {
+            RDKClasses().getClasses()
+        }
     }
 }
