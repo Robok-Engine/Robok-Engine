6a298f7beac32ece1a3653ffe73b9626f8b8d940
trindadedev13 <devsuay@gmail.com>
2024-09-09 23:34:17 -0300

    feat: Redesign About

--- feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/compose/ui/screens/about/AboutScreen.kt
+++ feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/compose/ui/screens/about/AboutScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.Image
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
