7cc2ca4fb6e164961c6dcc7756a78b9f73f971de
Aquiles Trindade <devsuay@gmail.com>
2024-09-10 18:53:44 -0300

    chore: ```Simplify``` template code of the ```MainScreen``` class

--- feature/feature-template/src/main/java/org/gampiot/robok/feature/template/code/android/game/logic/GameScreenLogicTemplate.kt
+++ feature/feature-template/src/main/java/org/gampiot/robok/feature/template/code/android/game/logic/GameScreenLogicTemplate.kt
@@ -21,24 +21,12 @@ open class GameScreenLogicTemplate : JavaClassTemplate {
             package ${getCodeClassPackageName()};
 
             import robok.game.screen.GameScreen;
-            import robok.game.gui.GUIViewListener;
-            import ${getCodeClassPackageName()}.datagui.MainGui;
 
-            public class ${getCodeClassName()} extends GameScreen implements GUIViewListener {
-
-                private MainGui views;
+            public class ${getCodeClassName()} extends GameScreen {
 
                 @Override
                 public void onScreenCreated() {
-                    views = MainGui.inflate(this);
-                    views.shootButton.setGUIViewListener(this);
-                }
-                
-                @Override
-                public void onClick(GUIView view) {
-                    if (view == views.shootButton) {
-                        
-                    }
+                    
                 }
             }
             """.trimIndent()
