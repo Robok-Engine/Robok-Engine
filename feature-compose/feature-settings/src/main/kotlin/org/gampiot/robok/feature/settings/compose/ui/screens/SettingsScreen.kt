67b908abbf66d51d6262a2de79cc4e8d12e49f8b
trindadedev13 <devsuay@gmail.com>
2024-09-09 22:03:52 -0300

    feat: Redesign About

--- feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/compose/ui/screens/SettingsScreen.kt
+++ feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/compose/ui/screens/SettingsScreen.kt
@@ -10,13 +10,10 @@ import androidx.navigation.NavController
 
 import org.koin.androidx.compose.koinViewModel
 
-import org.gampiot.robok.feature.component.compose.ApplicationScreen
-import org.gampiot.robok.feature.component.compose.appbars.TopBar
-import org.gampiot.robok.feature.component.compose.Title
+import org.gampiot.robok.feature.settings.compose.viewmodels.AppPreferencesViewModel
 import org.gampiot.robok.feature.component.compose.preferences.normal.Preference
 import org.gampiot.robok.feature.component.compose.preferences.base.PreferenceLayout
 import org.gampiot.robok.feature.component.compose.preferences.base.PreferenceGroup
-import org.gampiot.robok.feature.settings.compose.viewmodels.AppPreferencesViewModel
 import org.gampiot.robok.feature.res.Strings
 
 @OptIn(ExperimentalMaterial3Api::class)
