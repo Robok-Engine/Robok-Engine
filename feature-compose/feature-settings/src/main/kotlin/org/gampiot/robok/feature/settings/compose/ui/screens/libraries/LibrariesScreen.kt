90dd32038005f2b7cb31847ae3975d22a6ba4b15
trindadedev13 <devsuay@gmail.com>
2024-09-09 21:21:28 -0300

    feat: Redesign About

--- feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/compose/ui/screens/libraries/LibrariesScreen.kt
+++ feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/compose/ui/screens/libraries/LibrariesScreen.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
