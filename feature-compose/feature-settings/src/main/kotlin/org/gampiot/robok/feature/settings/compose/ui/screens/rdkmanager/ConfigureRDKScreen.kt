e818735151ac789437b39c42759da83cfcc22473
Aquiles Trindade <devsuay@gmail.com>
2024-09-11 14:23:02 -0300

    fix: ```launch``` not found

--- feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/compose/ui/screens/rdkmanager/ConfigureRDKScreen.kt
+++ feature-compose/feature-settings/src/main/kotlin/org/gampiot/robok/feature/settings/compose/ui/screens/rdkmanager/ConfigureRDKScreen.kt
@@ -14,6 +14,7 @@ import org.koin.core.parameter.parametersOf
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
