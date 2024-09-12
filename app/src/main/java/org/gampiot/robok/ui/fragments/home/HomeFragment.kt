7a328c0bc656052d9b53550301824a43c00e99c6
Aquiles Trindade <devsuay@gmail.com>
2024-09-12 19:24:49 -0300

    fix: Bundle import is ambigous

--- app/src/main/java/org/gampiot/robok/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/org/gampiot/robok/ui/fragments/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.content.Intent
 import android.net.Uri
-import android.os.Bundle
 import android.provider.DocumentsContract
 
 import androidx.activity.result.contract.ActivityResultContracts
