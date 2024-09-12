f0cd0484601a320dbe95f9703be4ee6c69c79f3f
trindadedev13 <devsuay@gmail.com>
2024-09-12 02:43:56 -0300

    fix: exclamation unexpected

--- app/src/main/java/org/gampiot/robok/ui/fragments/editor/EditorFragment.kt
+++ app/src/main/java/org/gampiot/robok/ui/fragments/editor/EditorFragment.kt
@@ -19,7 +19,7 @@ import io.github.rosemoe.sora.lang.diagnostic.DiagnosticRegion
 import org.gampiot.robok.R
 import org.gampiot.robok.databinding.FragmentEditorBinding
 import org.gampiot.robok.feature.editor.EditorListener
-import org.gampiot.robok.feature.component.terminal!!.RobokTerminal
+import org.gampiot.robok.feature.component.terminal.RobokTerminal
 import org.gampiot.robok.feature.res.Strings
 import org.gampiot.robok.feature.util.base.RobokFragment
 import org.gampiot.robok.feature.treeview.v2.provider.file
