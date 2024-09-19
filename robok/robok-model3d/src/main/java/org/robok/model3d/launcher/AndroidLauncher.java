8c156705ef90a16aa50d7c79d72c6d12c816a80d
Aquiles Trindade <devsuay@gmail.com>
2024-09-19 01:26:09 -0300

    fix: rename ActivityMain to Activity3dModel

--- robok/robok-model3d/src/main/java/org/robok/model3d/launcher/AndroidLauncher.java
+++ robok/robok-model3d/src/main/java/org/robok/model3d/launcher/AndroidLauncher.java
@@ -20,7 +20,7 @@ import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
-import org.robok.model3d.databinding.ActivityMainBinding;
+import org.robok.model3d.databinding.Activity3dModelBinding;
 import org.robok.model3d.objects.SceneObject;
 import org.robok.model3d.view.Model3DView;
 
