233ac1ae624628b8958c7fc63795284a6faebcfd
trindadedev13 <devsuay@gmail.com>
2024-09-21 06:44:12 -0300

    abc

--- feature-compose/feature-component/src/main/kotlin/org/gampiot/robok/feature/component/compose/dialog/RobokDialog.kt
+++ feature-compose/feature-component/src/main/kotlin/org/gampiot/robok/feature/component/compose/dialog/RobokDialog.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.material.icons.Icons
