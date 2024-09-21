f55f1558f2466aaea44b33335b5dc4fa11371f87
trindadedev13 <devsuay@gmail.com>
2024-09-21 14:26:43 -0300

    a

--- feature/feature-component/build.gradle.kts
+++ feature/feature-component/build.gradle.kts
@@ -31,23 +31,8 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 }
 
 dependencies {
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.13.0-alpha06")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    
-    val editorGroupId = "io.github.Rosemoe.sora-editor"
-    implementation(platform("$editorGroupId:bom:0.23.4"))
-    implementation("$editorGroupId:editor")
-    implementation("$editorGroupId:editor-lsp")
-    implementation("$editorGroupId:language-java")
-    implementation("$editorGroupId:language-textmate")
-    
-    val antlrVersion = "4.13.2"
-    implementation("org.antlr:antlr4:$antlrVersion") 
-    implementation("org.antlr:antlr4-runtime:$antlrVersion")
+    implementation(libs.material)
+    implementation(libs.appcompat)
     
     implementation(project(":feature:feature-res:strings"))
-    
-    implementation(project(":robok:robok-compiler"))
-    implementation(project(":robok:robok-diagnostic"))
 }
