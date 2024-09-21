a8303eebc027bec674bc16466bb4c88b991e8a64
trindadedev13 <devsuay@gmail.com>
2024-09-21 01:05:08 -0300

    idk

--- feature-compose/feature-component/src/main/kotlin/org/gampiot/robok/feature/component/compose/text/RobokText.kt
+++ feature-compose/feature-component/src/main/kotlin/org/gampiot/robok/feature/component/compose/text/RobokText.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.sp
 
 val NunitoFontFamily = FontFamily(
