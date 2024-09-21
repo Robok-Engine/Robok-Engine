a20100027a9392195c668bf69f6a56484db0acac
trindadedev13 <devsuay@gmail.com>
2024-09-21 14:09:20 -0300

    chore: formet code, move setCornerRadii to TInput

--- easy-components/src/main/java/dev/trindadedev/easyui/components/textfields/TInput.java
+++ easy-components/src/main/java/dev/trindadedev/easyui/components/textfields/TInput.java
@@ -18,72 +18,75 @@ import dev.trindadedev.easyui.components.R;
 
 public class TInput extends LinearLayout {
 
-    public TextInputLayout textInputLayout;
-    public TextInputEditText textInputEditText;
+     public TextInputLayout textInputLayout;
+     public TextInputEditText textInputEditText;
 
-    public TInput(Context context) {
-        this(context, null);
-    }
+     public TInput(Context context) {
+          this(context, null);
+     }
 
-    public TInput(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
+     public TInput(Context context, @Nullable AttributeSet attrs) {
+          this(context, attrs, 0);
+     }
 
-    public TInput(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        LayoutInflater.from(context).inflate(R.layout.layout_tinput_textfield, this, true);
-        textInputLayout = findViewById(R.id.text_input_layout);
-        textInputEditText = findViewById(R.id.text_input_edittext);
+     public TInput(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+          super(context, attrs, defStyleAttr);
+          LayoutInflater.from(context).inflate(R.layout.layout_tinput_textfield, this, true);
+          textInputLayout = findViewById(R.id.text_input_layout);
+          textInputEditText = findViewById(R.id.text_input_edittext);
 
-        if (attrs != null) {
-            TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TInput, defStyleAttr, 0);
+          if (attrs != null) {
+              TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TInput, defStyleAttr, 0);
 
-            textInputLayout.setHint(typedArray.getString(R.styleable.TInput_hint));
-            textInputLayout.setPlaceholderText(typedArray.getString(R.styleable.TInput_placeholderText));
+              textInputLayout.setHint(typedArray.getString(R.styleable.TInput_hint));
+              textInputLayout.setPlaceholderText(typedArray.getString(R.styleable.TInput_placeholderText));
 
-            String textString = typedArray.getString(R.styleable.TInput_text);
-            Editable editableText = !TextUtils.isEmpty(textString) ? Editable.Factory.getInstance().newEditable(textString) : null;
-            textInputEditText.setText(editableText);
+              String textString = typedArray.getString(R.styleable.TInput_text);
+              Editable editableText = !TextUtils.isEmpty(textString) ? Editable.Factory.getInstance().newEditable(textString) : null;
+              textInputEditText.setText(editableText);
 
-            int startIconDrawableRes = typedArray.getResourceId(R.styleable.TInput_startIconDrawable, 0);
-            if (startIconDrawableRes != 0) {
-                textInputLayout.setStartIconDrawable(ContextCompat.getDrawable(context, startIconDrawableRes));
-            }
+              int startIconDrawableRes = typedArray.getResourceId(R.styleable.TInput_startIconDrawable, 0);
+              if (startIconDrawableRes != 0) {
+                  textInputLayout.setStartIconDrawable(ContextCompat.getDrawable(context, startIconDrawableRes));
+              }
 
-            typedArray.recycle();
-        }
-    }
+              typedArray.recycle();
+          }
+     }
 
-    public CharSequence getHint() {
-        return textInputLayout.getHint();
-    }
+     public CharSequence getHint() {
+          return textInputLayout.getHint();
+     }
 
-    public void setHint(CharSequence hint) {
-        textInputLayout.setHint(hint);
-    }
+     public void setHint(CharSequence hint) {
+          textInputLayout.setHint(hint);
+     }
 
-    public CharSequence getPlaceholderText() {
-        return textInputLayout.getPlaceholderText();
-    }
+     public CharSequence getPlaceholderText() {
+          return textInputLayout.getPlaceholderText();
+     }
 
-    public void setPlaceholderText(CharSequence placeholderText) {
-        textInputLayout.setPlaceholderText(placeholderText);
-    }
+     public void setPlaceholderText(CharSequence placeholderText) {
+          textInputLayout.setPlaceholderText(placeholderText);
+     }
 
-    public int getStartIconDrawableRes() {
-        // Não há maneira direta de obter o recurso drawable. Esta é uma limitação.
-        return 0;
-    }
+     public void setCornerRadius(Float radii) {
+          textInputLayout.setBoxCornerRadii(radii, radii, radii, radii);
+     }
+    
+     public void setCornerRadius(Float p1, Float p2, Float p3, Float p4) {
+          textInputLayout.setBoxCornerRadii(p1, p2, p3, p4);
+     }
 
-    public void setStartIconDrawableRes(int drawableRes) {
-        textInputLayout.setStartIconDrawable(ContextCompat.getDrawable(getContext(), drawableRes));
-    }
+     public void setStartIconDrawableRes(int drawableRes) {
+          textInputLayout.setStartIconDrawable(ContextCompat.getDrawable(getContext(), drawableRes));
+     }
 
-    public CharSequence getText() {
-        return textInputEditText.getText();
-    }
+     public CharSequence getText() {
+          return textInputEditText.getText();
+     }
 
-    public void setText(CharSequence text) {
-        textInputEditText.setText(text);
-    }
+     public void setText(CharSequence text) {
+          textInputEditText.setText(text);
+     }
 }
