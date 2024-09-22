3a784a3971d7aad82e633a5943fa78ba5123671d
trindadedev13 <devsuay@gmail.com>
2024-09-21 22:54:20 -0300

    chore: add Xed-Editor (Karbon) copyright

--- feature/feature-treeview/src/main/java/org/gampiot/robok/feature/treeview/provider/DefaultFileIconProvider.kt
+++ feature/feature-treeview/src/main/java/org/gampiot/robok/feature/treeview/provider/DefaultFileIconProvider.kt
@@ -1,20 +1,20 @@
 package org.gampiot.robok.feature.treeview.provider
 
 /*
- *  This file is part of Robok © 2024.
+ *  This file is part of Xed-Editor (Karbon) © 2024.
  *
- *  Robok is free software: you can redistribute it and/or modify
+ *  Xed-Editor (Karbon) is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Robok is distributed in the hope that it will be useful,
+ *  Xed-Editor (Karbon) is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *   along with Robok.  If not, see <https://www.gnu.org/licenses/>.
+ *   along with Xed-Editor (Karbon).  If not, see <https://www.gnu.org/licenses/>.
  */ 
 
 import android.content.Context
