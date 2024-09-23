9e9ceb0c35a2508df7c2313f38b58997b95379ef
Aquiles Trindade <devsuay@gmail.com>
2024-09-23 00:02:42 -0300

    chore: move copyright to top

--- feature/feature-terminal/src/main/java/org/gampiot/robok/feature/terminal/TerminalActivity.kt
+++ feature/feature-terminal/src/main/java/org/gampiot/robok/feature/terminal/TerminalActivity.kt
@@ -1,5 +1,22 @@
 package org.gampiot.robok.feature.terminal
 
+/*
+*  This file is part of Robok © 2024.
+*
+*  Robok is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  Robok is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*   along with Robok.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.KeyEvent
@@ -21,23 +38,6 @@ import org.gampiot.robok.feature.terminal.databinding.LayoutDialogInputBinding
 import org.gampiot.robok.feature.util.KeyboardUtil
 import org.gampiot.robok.feature.util.base.RobokActivity
 import java.io.File
-
-/*
-*  This file is part of Robok © 2024.
-*
-*  Robok is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, either version 3 of the License, or
-*  (at your option) any later version.
-*
-*  Robok is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*   along with Robok.  If not, see <https://www.gnu.org/licenses/>.
-*/
 
 class TerminalActivity : RobokActivity(), TerminalSessionClient, TerminalViewClient {
   private var binding: ActivityTerminalBinding? = null
