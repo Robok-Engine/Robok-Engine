4bb16d86326055cd06bbc7d9d07e9d3975198cdb
trindadedev13 <devsuay@gmail.com>
2024-09-28 17:40:41 -0300

    fix: } expected in if

--- core/core-utils/src/main/java/org/gampiot/robok/core/utils/base/RobokActivity.kt
+++ core/core-utils/src/main/java/org/gampiot/robok/core/utils/base/RobokActivity.kt
@@ -132,7 +132,7 @@ open class RobokActivity : AppCompatActivity(), PermissionListener {
 
     override fun onReceive(status: Boolean) {
         if (status) {
-            if (Build.VERSION.SDK_INT >= 9
+            if (Build.VERSION.SDK_INT >= 9) {
               MaterialAlertDialogBuilder(this)
                 .setTitle(getString(Strings.error_storage_perm_title))
                 .setMessage(getString(Strings.error_storage_perm_message))
