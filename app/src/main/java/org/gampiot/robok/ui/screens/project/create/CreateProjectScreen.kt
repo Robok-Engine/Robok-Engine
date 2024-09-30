5c0e7841083f1742352b384638d379ceeb838773
trindadedev13 <devsuay@gmail.com>
2024-09-30 13:57:24 -0300

    fix: sp not found

--- app/src/main/java/org/gampiot/robok/ui/screens/project/create/CreateProjectScreen.kt
+++ app/src/main/java/org/gampiot/robok/ui/screens/project/create/CreateProjectScreen.kt
@@ -28,7 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
