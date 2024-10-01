ac9471646aec1785470a592859c15e69e3f68bef
trindadedev13 <devsuay@gmail.com>
2024-10-01 19:46:20 -0300

    fix: OutlineButton not exists
    the correct us OutlinedButton lol

--- core-compose/core-components/src/main/kotlin/org/gampiot/robok/core/components/compose/dialog/RobokDialog.kt
+++ core-compose/core-components/src/main/kotlin/org/gampiot/robok/core/components/compose/dialog/RobokDialog.kt
@@ -74,7 +74,7 @@ fun RobokDialog(
             }
         },
         dismissButton = {
-            OutlineButton(
+            OutlinedButton(
                 onClick = {
                     onDismissRequest()
                 }
