e496f06600c763a045e3b0fd1b57126e00e95b1b
trindadedev13 <devsuay@gmail.com>
2024-10-02 21:26:13 -0300

    fix: currentEditor not exists

--- app/src/main/java/org/robok/engine/ui/activities/editor/EditorActivity.kt
+++ app/src/main/java/org/robok/engine/ui/activities/editor/EditorActivity.kt
@@ -257,7 +257,7 @@ class EditorActivity : RobokActivity(), TabLayout.OnTabSelectedListener {
     private fun configureEditor() {
         binding.tabs.addOnTabSelectedListener(this)
         observeViewModel()
-        getCurrentEditor()?.let { editor ->
+        getCurrentEditor()?.let { currentEdior ->
             val antlrListener = object : AntlrListener {
                 override fun onDiagnosticStatusReceive(isError: Boolean) {
                     handler.removeCallbacks(diagnosticTimeoutRunnable)
