6ec10fe1399354c3f6eb287290ef0c259a9d9389
trindadedev13 <devsuay@gmail.com>
2024-10-05 15:13:14 -0300

    fix: IS_USE_MONET cannot be a val

--- feature/settings/src/main/kotlin/org/robok/engine/feature/settings/DefaultValues.kt
+++ feature/settings/src/main/kotlin/org/robok/engine/feature/settings/DefaultValues.kt
@@ -5,7 +5,7 @@ import android.os.Build.VERSION_CODES.S
 
 object DefaultValues {
     const val INSTALLED_RDK_VERSION = "RDK-1"
-    const val IS_USE_MONET = if (VERSION.SDK_INT >= VERSION_CODES.S) true else false
+    val IS_USE_MONET = VERSION.SDK_INT >= VERSION_CODES.S
     const val EDITOR_THEME = 0
     const val EDITOR_TYPEFACE = 0
     const val EDITOR_IS_USE_WORD_WRAP = false
