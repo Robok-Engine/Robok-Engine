ba730d3a8d3d7f2bbebc7ffbc4f408ed35bd941a
trindadedev13 <devsuay@gmail.com>
2024-10-05 18:39:26 -0300

    chore: clenup code

--- feature/editor/src/main/java/org/robok/engine/feature/editor/languages/java/store/RDKFileMapper.java
+++ feature/editor/src/main/java/org/robok/engine/feature/editor/languages/java/store/RDKFileMapper.java
@@ -17,13 +17,7 @@ package org.robok.engine.feature.editor.languages.java.store;
  *   along with Robok.  If not, see <https://www.gnu.org/licenses/>.
  */ 
 
-/*
-* RobokClasses
-* Class used to store robok classes
-* Only those necessary for development with Robok.
-* @author ThDev-Only
-*/
-
+import android.app.Activity;
 import android.content.Context;
 
 import java.io.File;
@@ -32,52 +26,51 @@ import java.net.URLClassLoader;
 import java.util.HashMap;
 
 public class RDKFileMapper {
-    
+
     private Context context;
     private HashMap<String, String> robokClasses;
-    private String atuallyRDK = "RDK-1";
+    private String actuallyRdk; 
     private File rdkDirectory;
-    
-    public RDKFileMapper(Context context){
+
+    public RDKFileMapper(Context context) {
         this.context = context;
         robokClasses = new HashMap<>();
-        rdkDirectory = new File(context.getFilesDir(), atuallyRDK + "/rdk/");
-        
+
+        actuallyRdk = "RDK-1"; 
+        rdkDirectory = new File(context.getFilesDir(), actuallyRdk + "/rdk/");
+
+        load();
     }
-    
-    public void load(){
+
+    public void load() {
         this.robokClasses = mapRdkClasses();
     }
-    
+
     public HashMap<String, String> getClasses() {
         return this.robokClasses;
     }
     
-    // Method that returns a HashMap with the names of the classes and their formatted directories
     public HashMap<String, String> mapRdkClasses() {
         File rdkFolder = rdkDirectory;
 
         if (rdkFolder.exists() && rdkFolder.isDirectory()) {
             mapClassesRecursively(rdkFolder, "", robokClasses);
-        }else{
-            robokClasses.put("ErrorRdkNaoExiste", "com.error.rdk.ErrorRdkNaoExiste");
+        } else {
+            robokClasses.put("RDKNotExistsException", "robok.rdk.RDKNotExistsException");
         }
 
         return robokClasses;
     }
 
-    // Recursive method to map .class classes into the directory
     private void mapClassesRecursively(File folder, String packageName, HashMap<String, String> robokClasses) {
         File[] files = folder.listFiles();
         if (files == null) return;
 
-        for (File file : files) {
+        for (File file: files) {
             if (file.isDirectory()) {
-                // Update package name as you cycle through folders
                 String newPackageName = packageName.isEmpty() ? file.getName() : packageName + "." + file.getName();
                 mapClassesRecursively(file, newPackageName, robokClasses);
             } else if (file.getName().endsWith(".class")) {
-                // Remove the .class extension and map the class
                 String className = file.getName().replace(".class", "");
                 String classPath = packageName + "." + className;
                 robokClasses.put(className, classPath);
@@ -85,42 +78,12 @@ public class RDKFileMapper {
         }
     }
 
-    //Method to create URLClassLoader from base directory
     public URLClassLoader getClassLoader() throws Exception {
         File file = rdkDirectory;
 
-        // Converts the base directory to a URL
         URL url = file.toURI().toURL();
         URL[] urls = new URL[]{url};
 
-        // Returns the URLClassLoader for loading classes
         return new URLClassLoader(urls);
     }
-
-    /*public static void main(String[] args) throws Exception {
-        // Diretório base onde os arquivos .class estão localizados
-        String rdkDirectory = "/caminho/para/sua/classe/";
-
-        // Cria o HashMap mapeando os arquivos .class
-        HashMap<String, String> robokClasses = 
-
-        // Exemplo de uso do HashMap
-        System.out.println(robokClasses.get("Physics")); // Exemplo: robok.physics.Physics
-
-        // Cria o URLClassLoader
-        URLClassLoader classLoader = getClassLoader(rdkDirectory);
-
-        // Carrega as classes com base no nome completo (pacote + nome da classe)
-        Class<?> class1 = classLoader.loadClass(robokClasses.get("Physics"));
-        Class<?> class2 = classLoader.loadClass(robokClasses.get("Graphic"));
-        Class<?> class3 = classLoader.loadClass(robokClasses.get("Util"));
-
-        // Exibe os nomes das classes carregadas
-        System.out.println("Classe 1: " + class1.getName());
-        System.out.println("Classe 2: " + class2.getName());
-        System.out.println("Classe 3: " + class3.getName());
-
-        // Fecha o classLoader após o uso
-        classLoader.close();
-    }*/
 }
