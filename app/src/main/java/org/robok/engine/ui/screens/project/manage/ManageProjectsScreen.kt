c3c77c4804433a52ff20c294fed2ee9c1d94f6df
trindadedev13 <devsuay@gmail.com>
2024-10-05 02:21:48 -0300

    fix: missing painterResource impory

--- app/src/main/java/org/robok/engine/ui/screens/project/manage/ManageProjectsScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/project/manage/ManageProjectsScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.Alignment
