9adc7195036a57082e62cfa9c2934912790b6e21
trindadedev13 <devsuay@gmail.com>
2024-10-05 00:39:14 -0300

    fix: clickable not found

--- app/src/main/java/org/robok/engine/ui/screens/project/manage/ManageProjectsScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/project/manage/ManageProjectsScreen.kt
@@ -22,6 +22,7 @@ import android.os.Environment
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
