6892a423994d10ba295a8b04122737f8d84a56ad
trindadedev13 <devsuay@gmail.com>
2024-10-07 19:51:00 -0300

    fix:

--- app/src/main/java/org/robok/engine/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/home/HomeScreen.kt
@@ -146,7 +146,7 @@ fun HomeScreen(
                                 navController.navigate(ProjectRoute)
                            }
                            2 -> {
-                                navController.navigate(SettingsRoutes)
+                                navController.navigate(SettingsRoute)
                            }
                            else -> onTerminalClicked(context)
                         }
