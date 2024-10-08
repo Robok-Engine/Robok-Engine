93231a86a1e84ac3e222b884e65ac7afc2f6c54d
trindadedev13 <devsuay@gmail.com>
2024-10-07 21:30:47 -0300

    fix:

--- app/src/main/java/org/robok/engine/routes/MainNavHost.kt
+++ app/src/main/java/org/robok/engine/routes/MainNavHost.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 
 import org.robok.engine.BuildConfig
 import org.robok.engine.models.project.ProjectTemplate
