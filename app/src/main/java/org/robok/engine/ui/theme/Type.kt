b25cb48c9115f6e9eb9a940578899290ccd8d822
trindadedev13 <devsuay@gmail.com>
2024-10-10 20:01:21 -0300

    fix: missing imports

--- app/src/main/java/org/robok/engine/ui/theme/Type.kt
+++ app/src/main/java/org/robok/engine/ui/theme/Type.kt
@@ -18,10 +18,12 @@ package org.robok.engine.ui.theme
  */
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import org.robok.engine.Fonts
 
 val NunitoFontFamily =
