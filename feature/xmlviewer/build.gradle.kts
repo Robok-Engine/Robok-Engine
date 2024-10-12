080151d664928b4cb48a776f618f2b5c702c64f5
trindadedev13 <devsuay@gmail.com>
2024-10-12 13:14:38 -0300

    a

---
+++ feature/xmlviewer/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.agp.lib)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "org.robok.engine.feature.xmlviewer"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+   
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
+    }
+
+    kotlinOptions {
+        jvmTarget = libs.versions.android.jvm.get()
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+}
