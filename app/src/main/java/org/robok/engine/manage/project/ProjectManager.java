7bf6c74fd397989c5af5fee013ab871dbf292b55
trindadedev13 <devsuay@gmail.com>
2024-10-16 23:33:19 -0300

    fix: use getAbosulutePath() instead File

--- app/src/main/java/org/robok/engine/manage/project/ProjectManager.java
+++ app/src/main/java/org/robok/engine/manage/project/ProjectManager.java
@@ -62,7 +62,7 @@ public class ProjectManager {
     }
     
     public String getProjectName() {
-        return getProjectPath().substring(getProjectPath().getAbsolutePath().lastIndexOf("/") + 1);
+        return getProjectPath().getAbsolute().substring(getProjectPath().getAbsolutePath().lastIndexOf("/") + 1);
     }
     
     public File getLibsPath() {
