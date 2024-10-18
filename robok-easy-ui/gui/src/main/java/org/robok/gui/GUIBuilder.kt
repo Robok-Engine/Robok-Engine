f55d6c707ae4fbaa64450eb359fb4ff30f5618ff
trindadedev13 <devsuay@gmail.com>
2024-10-18 15:05:09 -0300

    feat: add id in rootview

--- robok-easy-ui/gui/src/main/java/org/robok/gui/GUIBuilder.kt
+++ robok-easy-ui/gui/src/main/java/org/robok/gui/GUIBuilder.kt
@@ -54,7 +54,8 @@ class GUIBuilder(
         indentLevel++
         xmlCodeList.newLineBroken(DefaultValues.XMLNS(indent))
         xmlCodeList.newLineBroken("${indent}${DefaultValues.LAYOUT_HEIGHT}")
-        xmlCodeList.newLine("${indent}${DefaultValues.LAYOUT_WIDTH}")
+        xmlCodeList.newLineBroken("${indent}${DefaultValues.LAYOUT_WIDTH}")
+        xmlCodeList.newLine("${indent}android:id=\"@+id/root_view\"")
         xmlCodeList.newLineBroken(">")
         indentLevel++
     }
