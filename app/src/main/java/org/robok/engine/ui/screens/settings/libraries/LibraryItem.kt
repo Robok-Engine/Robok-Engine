1a0ddc6fbd41328d1c35762f08e5a79f428f5298
trindadedev13 <devsuay@gmail.com>
2024-10-20 10:51:25 -0300

    fix: clickable not found

--- app/src/main/java/org/robok/engine/ui/screens/settings/libraries/LibraryItem.kt
+++ app/src/main/java/org/robok/engine/ui/screens/settings/libraries/LibraryItem.kt
@@ -1,6 +1,7 @@
 package org.robok.engine.ui.screens.settings.libraries
 
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
