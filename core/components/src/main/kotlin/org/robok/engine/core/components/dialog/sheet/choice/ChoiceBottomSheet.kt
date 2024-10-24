de3b2de7c61b967c4b03094a5f8ce497bd65ff4d
Aquiles Trindade <devsuay@gmail.com>
2024-10-23 21:05:03 -0300

    fix: dp & Modifier not found

--- core/components/src/main/kotlin/org/robok/engine/core/components/dialog/sheet/choice/ChoiceBottomSheet.kt
+++ core/components/src/main/kotlin/org/robok/engine/core/components/dialog/sheet/choice/ChoiceBottomSheet.kt
@@ -21,6 +21,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import org.robok.engine.core.components.dialog.sheet.BottomSheetContent
 import org.robok.engine.core.components.radio.IntRadioController
