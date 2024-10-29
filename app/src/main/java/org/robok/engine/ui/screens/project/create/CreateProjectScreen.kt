e6d4cbd746d6dc07f7857004e05a514faffe4e16
trindadedev13 <devsuay@gmail.com>
2024-10-29 05:55:03 -0300

    fix: crash when createProject & Open editor because projectPath is null

--- app/src/main/java/org/robok/engine/ui/screens/project/create/CreateProjectScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/project/create/CreateProjectScreen.kt
@@ -57,6 +57,7 @@ import org.robok.engine.strings.Strings
 import org.robok.engine.ui.activities.editor.EditorActivity
 import org.robok.engine.ui.screens.project.create.state.CreateProjectState
 import org.robok.engine.ui.screens.project.create.viewmodel.CreateProjectViewModel
+import org.robok.engine.keys.ExtraKeys
 
 @Composable
 fun CreateProjectScreen(template: ProjectTemplate) {
@@ -127,7 +128,7 @@ private fun Screen(
                     onSuccess = {
                         val bundle =
                             android.os.Bundle().apply {
-                                putString("projectPath", viewModel.getProjectPath().absolutePath)
+                                putString(ExtraKeys.Project.PATH, viewModel.getProjectPath().absolutePath)
                             }
                         val intent =
                             Intent(context, EditorActivity::class.java).apply { putExtras(bundle) }
