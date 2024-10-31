83c7a2b5ffe1b84c2b1b9729fc95a84b642f9d13
trindadedev13 <devsuay@gmail.com>
2024-10-31 03:26:05 +0000

    chore: Format Code
    format code with ktfmt in github actions

--- app/src/main/java/org/robok/engine/ui/animations/navigation/NavigationAnimationTransitions.kt
+++ app/src/main/java/org/robok/engine/ui/animations/navigation/NavigationAnimationTransitions.kt
@@ -16,24 +16,20 @@ package org.robok.engine.ui.animations.navigation
  *  You should have received a copy of the GNU General Public License
  *   along with Robok.  If not, see <https://www.gnu.org/licenses/>.
  */
- 
+
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.core.tween
 
 object NavigationAnimationTransitions {
 
-    val enterTransition =
-        fadeIn(tween(250)) + slideInHorizontally { it / 2 }
-        
-    val exitTransition =
-        fadeOut(tween(200)) + slideOutHorizontally { -it / 2 }
+    val enterTransition = fadeIn(tween(250)) + slideInHorizontally { it / 2 }
 
-    val popEnterTransition =
-        fadeIn(tween(250)) + slideInHorizontally { -it / 2 }
+    val exitTransition = fadeOut(tween(200)) + slideOutHorizontally { -it / 2 }
 
-    val popExitTransition =
-        fadeOut(tween(200)) + slideOutHorizontally { it / 2 }
+    val popEnterTransition = fadeIn(tween(250)) + slideInHorizontally { -it / 2 }
+
+    val popExitTransition = fadeOut(tween(200)) + slideOutHorizontally { it / 2 }
 }
