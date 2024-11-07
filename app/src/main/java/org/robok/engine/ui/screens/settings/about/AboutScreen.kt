f5c1969d63b6932e2cc983d30068aa1a58be2c28
trindadedev13 <devsuay@gmail.com>
2024-11-06 23:56:41 -0300

    fix: AlertDialog not found

--- app/src/main/java/org/robok/engine/ui/screens/settings/about/AboutScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/settings/about/AboutScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.AlertDialog
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
