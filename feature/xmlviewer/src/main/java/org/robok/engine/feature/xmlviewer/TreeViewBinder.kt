bb233905a648238bca0b34f57bbfeca1eab2b5eb
trindadedev13 <devsuay@gmail.com>
2024-11-06 21:45:06 -0300

    fix: no value passed in TreeNode

--- feature/xmlviewer/src/main/java/org/robok/engine/feature/xmlviewer/TreeViewBinder.kt
+++ feature/xmlviewer/src/main/java/org/robok/engine/feature/xmlviewer/TreeViewBinder.kt
@@ -5,10 +5,10 @@ import androidx.annotation.IdRes
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class TreeViewBinder<VH : RecyclerView.ViewHolder> : LayoutItemType {
-  
+
   abstract fun provideViewHolder(itemView: View): VH
 
-  abstract fun bindView(holder: VH, position: Int, node: TreeNode)
+  abstract fun bindView(holder: VH, position: Int, node: TreeNode<LayoutItemType>)
 
   open class ViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView) {
     protected fun <T : View> findViewById(@IdRes id: Int): T {
