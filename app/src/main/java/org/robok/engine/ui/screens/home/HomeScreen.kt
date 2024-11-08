7c8a0a29bdd9f1ffa63fd37cd38d154112ea2f84
trindadedev13 <devsuay@gmail.com>
2024-11-08 14:03:13 -0300

    feat: Improve HomeScreen

--- app/src/main/java/org/robok/engine/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/home/HomeScreen.kt
@@ -21,49 +21,22 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.DocumentsContract
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.*
+import androidx.compose.foundation.shape.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material.icons.rounded.FolderOpen
-import androidx.compose.material.icons.rounded.Info
-import androidx.compose.material.icons.rounded.Settings
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.vector.*
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.res.*
+import androidx.compose.ui.text.font.*
+import androidx.compose.ui.unit.*
 import java.io.File
 import org.robok.engine.Drawables
 import org.robok.engine.core.utils.PathUtils
@@ -83,19 +56,6 @@ fun HomeScreen() {
   val navController = LocalMainNavController.current
   val context = LocalContext.current
 
-  var selectedFolderUri by remember { mutableStateOf<Uri?>(null) }
-
-  val folderPickerLauncher =
-    rememberLauncherForActivityResult(contract = ActivityResultContracts.OpenDocumentTree()) {
-      uri: Uri? ->
-      selectedFolderUri = uri
-      if (uri != null) {
-        val bundle = Bundle().apply { putString("projectPath", processUri(context, uri)) }
-        val intent = Intent(context, EditorActivity::class.java).apply { putExtras(bundle) }
-        context.startActivity(intent)
-      }
-    }
-
   Column(
     modifier = Modifier.fillMaxWidth().fillMaxHeight(),
     horizontalAlignment = Alignment.CenterHorizontally,
@@ -112,7 +72,7 @@ fun HomeScreen() {
         colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
       )
       Text(
-        text = "Robok",
+        text = BuildConfig.APP_NAME,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
         color = MaterialTheme.colorScheme.onSurface,
@@ -166,34 +126,15 @@ fun HomeScreen() {
   }
 }
 
-private fun onOpenProjectClicked(folderPickerLauncher: ActivityResultLauncher<Uri?>) {
-  folderPickerLauncher.launch(null)
-}
-
-private fun processUri(ac: Context, uri: Uri): String {
-  ac.contentResolver.takePersistableUriPermission(
-    uri,
-    Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION,
-  )
-  val documentId = DocumentsContract.getTreeDocumentId(uri)
-  val folderUri = DocumentsContract.buildDocumentUriUsingTree(uri, documentId)
-  val path = PathUtils.convertUriToPath(ac, folderUri)
-  if (File(path).exists().not()) {
-    return "${getDefaultPath()}/Robok"
-  }
-  return path
-}
-
 @Composable
 fun HomeCardItem(icon: ImageVector, title: String, onClick: () -> Unit) {
   Card(
-    modifier =
-      Modifier.padding(8.dp)
-        .clickable(onClick = onClick)
-        .clip(RoundedCornerShape(17.dp))
-        .height(100.dp),
-    shape = RoundedCornerShape(17.dp),
+    modifier =Modifier
+      .padding(8.dp)
+      .height(100.dp),
+    shape = MaterialTheme.shapes.medium,
     elevation = CardDefaults.cardElevation(0.dp),
+    onClick = onClick
   ) {
     Column(
       modifier = Modifier.padding(11.dp).fillMaxSize(),
