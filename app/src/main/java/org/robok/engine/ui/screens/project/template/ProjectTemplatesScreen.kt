99d96df261b7f9fb4c50d80d24729add23a0de4f
trindadedev13 <devsuay@gmail.com>
2024-11-08 11:47:09 -0300

    refactor: use manually grid instead LazyVerticalGrid

--- app/src/main/java/org/robok/engine/ui/screens/project/template/ProjectTemplatesScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/project/template/ProjectTemplatesScreen.kt
@@ -17,42 +17,20 @@
 
 package org.robok.engine.ui.screens.project.template
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.surfaceColorAtElevation
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.input.nestedscroll.*
+import androidx.compose.ui.layout.*
+import androidx.compose.ui.res.*
+import androidx.compose.ui.text.font.*
+import androidx.compose.ui.unit.*
 import org.robok.engine.Drawables
 import org.robok.engine.defaults.DefaultTemplate
 import org.robok.engine.models.project.ProjectTemplate
@@ -85,43 +63,53 @@ fun ProjectTemplatesScreen(onTemplateClick: (ProjectTemplate) -> Unit) {
     },
   ) { innerPadding ->
     Box(modifier = Modifier.padding(innerPadding).padding(16.dp)) {
-      LazyVerticalGrid(columns = GridCells.Fixed(2)) {
-        items(templates) {
-          Card(
-            modifier = Modifier.heightIn(max = 250.dp),
-            colors =
-              CardDefaults.cardColors()
-                .copy(containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)),
-            shape = MaterialTheme.shapes.medium,
-            onClick = { onTemplateClick(it) },
+      Column {
+        templates.chunked(2).forEach { rowItems ->
+          Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
           ) {
-            Box(
-              modifier = Modifier.fillMaxHeight().weight(1f),
-              contentAlignment = Alignment.Center,
-            ) {
-              Image(
-                painter = painterResource(id = Drawables.splash_bg),
-                contentScale = ContentScale.Crop,
-                contentDescription = null,
-              )
-
-              Image(painter = painterResource(id = Drawables.ic_robok), contentDescription = null)
+            rowItems.forEach { template ->
+              ProjectTemplateCard(template, onTemplateClick)
             }
-
-            ListItem(
-              overlineContent = {
-                Text(
-                  text = if (it.javaSupport) "Java" else if (it.kotlinSupport) "Kotlin" else "",
-                  fontWeight = FontWeight.Bold,
-                )
-              },
-              headlineContent = { Text(it.name) },
-              supportingContent = { Text(it.packageName) },
-              colors = ListItemDefaults.colors(containerColor = Color.Transparent),
-            )
           }
         }
       }
     }
+  }
+}
+
+@Composable
+fun ProjectTemplateCard(template: ProjectTemplate, onTemplateClick: (ProjectTemplate) -> Unit) {
+  Card(
+    modifier = Modifier.weight(1f).heightIn(max = 250.dp),
+    colors = CardDefaults.cardColors().copy(containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)),
+    shape = MaterialTheme.shapes.medium,
+    onClick = { onTemplateClick(template) },
+  ) {
+    Box(
+      modifier = Modifier.fillMaxHeight().weight(1f),
+      contentAlignment = Alignment.Center,
+    ) {
+      Image(
+        painter = painterResource(id = Drawables.splash_bg),
+        contentScale = ContentScale.Crop,
+        contentDescription = null,
+      )
+
+      Image(painter = painterResource(id = Drawables.ic_robok), contentDescription = null)
+    }
+
+    ListItem(
+      overlineContent = {
+        Text(
+          text = if (template.javaSupport) "Java" else if (template.kotlinSupport) "Kotlin" else "",
+          fontWeight = FontWeight.Bold,
+        )
+      },
+      headlineContent = { Text(template.name) },
+      supportingContent = { Text(template.packageName) },
+      colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+    )
   }
 }
