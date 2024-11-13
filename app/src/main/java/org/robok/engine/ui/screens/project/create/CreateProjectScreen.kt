e6e766b1533a09c65078a1a9038073df19797797
trindadedev13 <devsuay@gmail.com>
2024-11-13 10:15:35 -0300

    fix: Error Icon not found

--- app/src/main/java/org/robok/engine/ui/screens/project/create/CreateProjectScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/project/create/CreateProjectScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Error
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
