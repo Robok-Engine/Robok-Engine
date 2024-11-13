64740c39240d1332f43895052ba065f8ec75369d
trindadedev13 <devsuay@gmail.com>
2024-11-13 12:24:04 -0300

    debug: Logs

--- app/src/main/java/org/robok/engine/ui/screens/settings/about/repository/ContributorsRepository.kt
+++ app/src/main/java/org/robok/engine/ui/screens/settings/about/repository/ContributorsRepository.kt
@@ -3,6 +3,7 @@ package org.robok.engine.ui.screens.settings.about.repository
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import org.robok.engine.core.utils.RobokLog
 import org.robok.engine.models.about.Contributor
 
 class ContributorsRepository(
@@ -16,16 +17,19 @@ class ContributorsRepository(
 
       if (response.status.value in 200..299) {
         val contributors: List<Contributor> = response.body()
-        contributors.filter { contributor ->
+        val c = contributors.filter { contributor ->
           contributor.type != "Bot" &&
           contributor.role != "Bot" &&
           contributor.user_view_type != "private"
         }
+        RobokLog.e(message = c.toString())
+        c
       } else {
         emptyList()
       }
       
     } catch (e: Exception) {
+      RobokLog.e(message = e.toString())
       emptyList()
     }
   }
