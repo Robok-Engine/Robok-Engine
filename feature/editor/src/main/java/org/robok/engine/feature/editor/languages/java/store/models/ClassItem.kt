71b35bf36d3da66012c8a542bf255f01e7e5b2a2
trindadedev13 <devsuay@gmail.com>
2024-11-17 15:22:44 -0300

    chore: remove unused ClassItem.kt

--- feature/editor/src/main/java/org/robok/engine/feature/editor/languages/java/store/models/ClassItem.kt
+++
@@ -1,5 +0,0 @@
-package org.robok.engine.feature.editor.languages.java.store.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable data class ClassItem(val className: String, val classPackageName: String)
