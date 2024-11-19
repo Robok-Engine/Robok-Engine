956008faf29001d6463f0893f236a0b09baa6efd
trindadedev13 <devsuay@gmail.com>
2024-11-19 01:38:39 -0300

    fix: Verify if list is empty before

--- app/src/main/java/org/robok/engine/ui/screens/settings/debug/logging/SettingsDebugLoggingScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/settings/debug/logging/SettingsDebugLoggingScreen.kt
@@ -18,22 +18,66 @@ package org.robok.engine.ui.screens.settings.debug.logging
  */
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import org.robok.engine.strings.Strings
+import org.robok.engine.Drawables
 import org.robok.engine.core.utils.RobokLog
 import org.robok.engine.core.components.Screen
 import org.robok.engine.core.components.preferences.base.PreferenceGroup
+import org.robok.engine.core.components.preferences.base.PreferenceTemplate
 
 @Composable
 fun SettingsDebugLoggingScreen() {
+  val logs by rememberLogs()
   Screen(label = stringResource(id = Strings.text_logging)) {
-    PreferenceGroup(heading = stringResource(id = Strings.text_all_logs)) {
-      RobokLog.getLogs().forEach { log ->
-        Text(
-          text = log
-        )
+    PreferenceGroup(heading = stringResource(id = Strings.text_all_logs)) { 
+      if (logs.isEmpty().not()) {
+        logs.forEach { log ->
+          Text(
+            text = log
+          )
+        }
+      } else {
+        EmptyContentItem()
       }
     }
   }
+}
+
+@Composable
+private fun EmptyContentItem() {
+  PreferenceTemplate(
+    title = {
+      Text(
+        text = stringResource(id = Strings.warning_no_logs),
+        style = MaterialTheme.typography.titleMedium,
+      )
+    },
+    startWidget = {
+      Image(
+        painter = painterResource(id = Drawables.ic_warning_24),
+        contentDescription = null,
+        modifier = Modifier.size(32.dp).clip(CircleShape)
+      )
+    },
+    modifier = Modifier.fillMaxWidth()
+  )
+}
+
+
+@Composable
+private fun rememberLogs() = remember {
+  RobokLog.getLogs()
 }
