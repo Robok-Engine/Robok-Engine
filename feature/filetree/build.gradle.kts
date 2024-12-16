f0e7c59272a729d2a2061203c9baa13002267423
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-16 17:58:43 -0300

    fix: missing recyclerview

--- feature/filetree/build.gradle.kts
+++ feature/filetree/build.gradle.kts
@@ -1,28 +1,29 @@
 plugins {
-    alias(libs.plugins.agp.lib)
-    alias(libs.plugins.kotlin)
+  alias(libs.plugins.agp.lib)
+  alias(libs.plugins.kotlin)
 }
 
 android {
-    namespace = "org.robok.engine.feature.filetree"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
+  namespace = "org.robok.engine.feature.filetree"
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
     
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+  defaultConfig {
+    minSdk = libs.versions.android.minSdk.get().toInt()
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
    
-    compileOptions {
-        sourceCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
-        targetCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
-    }
+  compileOptions {
+    sourceCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
+    targetCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
+  }
 
-    kotlinOptions {
-        jvmTarget = libs.versions.android.jvm.get()
-    }
+  kotlinOptions {
+    jvmTarget = libs.versions.android.jvm.get()
+  }
 }
 
 dependencies {
-    implementation(libs.androidx.appcompat)
-    implementation(libs.google.material)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.google.material)
+  implementation(libs.androidx.recyclerview)
 }
