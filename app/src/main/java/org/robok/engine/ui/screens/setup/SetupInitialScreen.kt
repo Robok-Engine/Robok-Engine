4a8bc34299038cb1000746f7de46ab5a54e89277
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-16 02:55:52 -0300

    fix: sp not found

--- app/src/main/java/org/robok/engine/ui/screens/setup/SetupInitialScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/setup/SetupInitialScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.draw.clip
 import org.robok.engine.Drawables
 import org.robok.engine.Strings
