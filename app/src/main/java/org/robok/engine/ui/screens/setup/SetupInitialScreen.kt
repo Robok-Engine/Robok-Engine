47a1685bf90bc06cc9b6b68236f857d729f22579
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-16 02:53:08 -0300

    fix: bottomButtons not imported

--- app/src/main/java/org/robok/engine/ui/screens/setup/SetupInitialScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/setup/SetupInitialScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.draw.clip
+import org.robok.engine.ui.screens.setup.components.BottomButtons
 
 @Composable
 fun SetupInitialScreen(
