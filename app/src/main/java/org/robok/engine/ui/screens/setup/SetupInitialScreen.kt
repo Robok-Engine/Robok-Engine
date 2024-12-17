6a04f9c1506a809d327977cc666a9e0200f2f956
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-16 21:23:02 -0300

    fix: Box not imported

--- app/src/main/java/org/robok/engine/ui/screens/setup/SetupInitialScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/setup/SetupInitialScreen.kt
@@ -20,6 +20,7 @@ package org.robok.engine.ui.screens.setup
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
