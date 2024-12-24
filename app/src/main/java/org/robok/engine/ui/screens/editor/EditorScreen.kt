e59a7cba16f4a4262bf41794a2e23698993c962e
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-24 02:23:54 -0300

    fix: missing Modifier importation

--- app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import java.io.File
