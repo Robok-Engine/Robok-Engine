269a89b9952eca88592d6659f8e80fee31e88e3f
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-24 03:07:34 -0300

    fix: missing Spacer not imported

--- app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.More
 import androidx.compose.material.icons.rounded.PlayArrow
