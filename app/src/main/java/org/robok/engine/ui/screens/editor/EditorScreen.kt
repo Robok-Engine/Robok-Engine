4622a046f72ff7d6b6e0f780832654f849105740
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-24 02:23:24 -0300

    fix: wrong typo in ModalDrawerSheet import

--- app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
@@ -30,7 +30,7 @@ import androidx.compose.material3.contentColorFor
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.MaterialTheme
-import androidx.comoose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
