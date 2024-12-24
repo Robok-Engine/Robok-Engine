a467b0c2e32410a104c51e54f83ffbdb8eda9d7b
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-24 03:08:01 -0300

    fix: missing height not imported

--- app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Spacer
