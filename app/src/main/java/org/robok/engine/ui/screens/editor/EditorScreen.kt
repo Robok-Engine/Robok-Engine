3493a69861a34e37838a050bd51261aa5f13660e
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-24 03:07:04 -0300

    fix: missing Text not imported

--- app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.rememberCoroutineScope
