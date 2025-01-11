647bf8c89e130cd2f6030121b440b886e97170b6
SyntaxSpin <86142168+SyntaxSpins@users.noreply.github.com>
2025-01-11 04:49:04 +0100

    ui: Change Setup Welcome Step image to a person. (#391)
    * ui : redesigning setupWelcomScreen
    
    * fix: Welcoming screen vector issue
    
    * fix: Welcoming screen vector issue
    
    * fix: Welcoming screen vector issue
    
    * fix: Welcimage size
    
    * fix: Welcimage size and texts not showing
    
    * fix: text not showing
    
    * Refactor : Changing Sapien fiel name

--- app/src/main/java/org/robok/engine/ui/screens/setup/SetupWelcomeScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/setup/SetupWelcomeScreen.kt
@@ -75,10 +75,12 @@ fun SetupWelcomeScreen(onBack: () -> Unit, onNext: () -> Unit) {
 fun Welcome(modifier: Modifier = Modifier) {
   Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
     Image(
-      painter = painterResource(id = Drawables.ic_launcher),
+      painter = painterResource(id = Drawables.ic_setup_welcome_person ),
       contentDescription = "App Icon",
-      modifier = Modifier.size(256.dp).clip(CircleShape),
-      contentScale = ContentScale.Crop,
+      modifier = Modifier.size(256.dp)
+      .padding(bottom = 24.dp)
+      ,
+      contentScale = ContentScale.Fit,
     )
     Text(
       text = stringResource(id = Strings.setup_welcome_title),
