ca1a5b8b2b18324c6635312b0a937bbf4277aa12
trindadedev13 <trindadedev13@gmail.com>
2025-01-21 22:43:35 -0300

    fix: Fix Typo

--- core/components/src/main/kotlin/org/robok/engine/core/components/dialog/loading/LoadingDialog.kt
+++ core/components/src/main/kotlin/org/robok/engine/core/components/dialog/loading/LoadingDialog.kt
@@ -23,7 +23,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
