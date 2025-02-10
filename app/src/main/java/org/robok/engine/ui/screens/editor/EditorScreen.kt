19d05682e902f2c5ee1d5e0052e2f7029095297f
trindadedev13 <trindadedev13@gmail.com>
2025-02-10 20:12:30 -0300

    feat: improve back dialog in editor

--- app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/editor/EditorScreen.kt
@@ -37,9 +37,9 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -102,7 +102,7 @@ fun EditorScreen(pPath: String) {
         }
       },
       dismissButton = {
-        TextButton(onClick = { navController.popBackStack() }) {
+        OutlinedButton(onClick = { navController.popBackStack() }) {
           Text(text = stringResource(Strings.text_exit_without_save))
         }
       },
