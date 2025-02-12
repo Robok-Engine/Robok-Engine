ccc83dcb45460f34cac004428e96ce4e6282ff68
trindadedev13 <trindadedev13@gmail.com>
2025-02-12 14:02:29 -0300

    fix: Card not found

--- app/src/main/java/org/robok/engine/ui/activities/debug/AppFailureActivity.kt
+++ app/src/main/java/org/robok/engine/ui/activities/debug/AppFailureActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
