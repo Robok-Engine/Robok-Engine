4fe4940d3105f768282ef84f56a4ddb59df8e103
trindadedev13 <trindadedev13@gmail.com>
2025-03-13 16:20:49 +0000

    chore: Format Kotlin Code
    format code with ktfmt in github actions

--- app/src/main/java/org/robok/engine/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/home/HomeScreen.kt
@@ -63,103 +63,94 @@ import org.robok.engine.ui.theme.Typography
 
 @Composable
 fun HomeScreen() {
-    val navController = LocalMainNavController.current
-    val context = LocalContext.current
-  
-    val homeCardItems = listOf(
-        HomeCardItemData(
-            icon = Icons.Rounded.Add,
-            title = stringResource(id = Strings.title_create_project),
-            onClick = {
-                navController.navigateSingleTop(route = TemplatesRoute)
-            }
-        ),
-        HomeCardItemData(
-            icon = Icons.Rounded.FolderOpen,
-            title = stringResource(id = Strings.title_open_project),
-            onClick = {
-                navController.navigateSingleTop(route = ManageProjectsRoute)
-            }
-        ),
-        HomeCardItemData(
-            icon = Icons.Rounded.Settings,
-            title = stringResource(id = Strings.common_word_settings),
-            onClick = {
-                navController.navigateSingleTop(route = SettingsRoute)
-            }
-        ),
-        HomeCardItemData(
-            icon = Icons.Rounded.Info,
-            title = stringResource(id = Strings.title_terminal),
-            onClick = {
-                navController.navigateSingleTop(route = TerminalRoute)
-            }
-        )
+  val navController = LocalMainNavController.current
+  val context = LocalContext.current
+
+  val homeCardItems =
+    listOf(
+      HomeCardItemData(
+        icon = Icons.Rounded.Add,
+        title = stringResource(id = Strings.title_create_project),
+        onClick = { navController.navigateSingleTop(route = TemplatesRoute) },
+      ),
+      HomeCardItemData(
+        icon = Icons.Rounded.FolderOpen,
+        title = stringResource(id = Strings.title_open_project),
+        onClick = { navController.navigateSingleTop(route = ManageProjectsRoute) },
+      ),
+      HomeCardItemData(
+        icon = Icons.Rounded.Settings,
+        title = stringResource(id = Strings.common_word_settings),
+        onClick = { navController.navigateSingleTop(route = SettingsRoute) },
+      ),
+      HomeCardItemData(
+        icon = Icons.Rounded.Info,
+        title = stringResource(id = Strings.title_terminal),
+        onClick = { navController.navigateSingleTop(route = TerminalRoute) },
+      ),
     )
 
+  Column(
+    modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center,
+  ) {
     Column(
-        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+      modifier = Modifier.fillMaxWidth().padding(8.dp),
+      horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth().padding(8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Image(
-                painter = painterResource(id = Drawables.ic_robok),
-                contentDescription = null,
-                modifier = Modifier.size(70.dp).padding(8.dp),
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
-            )
-            Text(
-                text = stringResource(Strings.app_name),
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp,
-                color = MaterialTheme.colorScheme.onSurface,
-                modifier = Modifier.padding(8.dp),
-            )
-        }
-
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            contentPadding = PaddingValues(8.dp),
-            modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
-        ) {
-            items(homeCardItems) { item ->
-                HomeCardItem(item = item)
-            }
-        }
+      Image(
+        painter = painterResource(id = Drawables.ic_robok),
+        contentDescription = null,
+        modifier = Modifier.size(70.dp).padding(8.dp),
+        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+      )
+      Text(
+        text = stringResource(Strings.app_name),
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        color = MaterialTheme.colorScheme.onSurface,
+        modifier = Modifier.padding(8.dp),
+      )
     }
+
+    LazyVerticalGrid(
+      columns = GridCells.Fixed(2),
+      contentPadding = PaddingValues(8.dp),
+      modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+    ) {
+      items(homeCardItems) { item -> HomeCardItem(item = item) }
+    }
+  }
 }
 
 @Composable
 fun HomeCardItem(item: HomeCardItemData) {
-    Card(
-        modifier = Modifier.padding(8.dp).height(100.dp),
-        shape = MaterialTheme.shapes.medium,
-        elevation = CardDefaults.cardElevation(0.dp),
-        onClick = item.onClick,
+  Card(
+    modifier = Modifier.padding(8.dp).height(100.dp),
+    shape = MaterialTheme.shapes.medium,
+    elevation = CardDefaults.cardElevation(0.dp),
+    onClick = item.onClick,
+  ) {
+    Column(
+      modifier = Modifier.padding(11.dp).fillMaxSize(),
+      horizontalAlignment = Alignment.Start,
+      verticalArrangement = Arrangement.Top,
     ) {
-        Column(
-            modifier = Modifier.padding(11.dp).fillMaxSize(),
-            horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.Top,
-        ) {
-            Image(
-                imageVector = item.icon,
-                contentDescription = item.title,
-                modifier = Modifier.size(25.dp),
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
-            )
-            Text(text = item.title, modifier = Modifier.padding(top = 8.dp), style = Typography.bodyMedium)
-        }
+      Image(
+        imageVector = item.icon,
+        contentDescription = item.title,
+        modifier = Modifier.size(25.dp),
+        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+      )
+      Text(
+        text = item.title,
+        modifier = Modifier.padding(top = 8.dp),
+        style = Typography.bodyMedium,
+      )
     }
+  }
 }
 
 @Immutable
-data class HomeCardItemData(
-    val icon: ImageVector,
-    val title: String,
-    val onClick: () -> Unit
-)
+data class HomeCardItemData(val icon: ImageVector, val title: String, val onClick: () -> Unit)
