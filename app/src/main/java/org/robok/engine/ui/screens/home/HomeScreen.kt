1a706b28e049267661b089d4ddb58fd0e8825889
Aquiles Trindade <trindadedev13@gmail.com>
2025-03-13 13:17:37 -0300

    Update HomeScreen.kt

--- app/src/main/java/org/robok/engine/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/org/robok/engine/ui/screens/home/HomeScreen.kt
@@ -1,20 +1,39 @@
 package org.robok.engine.ui.screens.home
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.*
-import androidx.compose.foundation.shape.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.*
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.FolderOpen
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.robok.engine.Drawables
 import org.robok.engine.Strings
 import org.robok.engine.ext.navigateSingleTop
@@ -91,9 +110,7 @@ fun HomeScreen() {
             modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
         ) {
             items(homeCardItems) { item ->
-                HomeCardItem(
-                    item = item
-                )
+                HomeCardItem(item = item)
             }
         }
     }
@@ -113,12 +130,12 @@ fun HomeCardItem(item: HomeCardItemData) {
             verticalArrangement = Arrangement.Top,
         ) {
             Image(
-                imageVector = homeCardItemData.icon,
-                contentDescription = homeCardItemData.title,
+                imageVector = item.icon,
+                contentDescription = item.title,
                 modifier = Modifier.size(25.dp),
                 colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
             )
-            Text(text = homeCardItemData.title, modifier = Modifier.padding(top = 8.dp), style = Typography.bodyMedium)
+            Text(text = item.title, modifier = Modifier.padding(top = 8.dp), style = Typography.bodyMedium)
         }
     }
 }
