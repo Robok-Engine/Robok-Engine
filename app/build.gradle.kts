ef8e5beeba2fc1fa18c48aad26cd76bf8631aea9
trindadedev13 <trindadedev13@gmail.com>
2025-04-03 14:57:30 -0300

    feat: Enable Minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 
   buildTypes {
     getByName("release") {
-      isMinifyEnabled = false
+      isMinifyEnabled = true
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
     getByName("debug") {
