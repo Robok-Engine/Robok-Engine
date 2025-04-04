0c600172e32213109b450c992ea808c48a4da281
trindadedev13 <trindadedev13@gmail.com>
2025-04-04 02:12:50 -0300

    .

--- core/settings/src/main/kotlin/org/robok/engine/core/settings/DefaultValues.kt
+++ core/settings/src/main/kotlin/org/robok/engine/core/settings/DefaultValues.kt
@@ -6,7 +6,7 @@ import android.os.Build.VERSION_CODES
 object DefaultValues {
   val IS_USE_MONET = VERSION.SDK_INT >= VERSION_CODES.S
   const val IS_USE_AMOLED = false
-  const val APP_THEME_SEED_COLOR: Int = 0xFF4C662B
+  const val APP_THEME_SEED_COLOR: Int = 0xFF4C662B.toInt()
   const val APP_THEME_PALETTE_STYLE_INDEX = 0
   const val EDITOR_THEME = 0
   const val EDITOR_TYPEFACE = 0
