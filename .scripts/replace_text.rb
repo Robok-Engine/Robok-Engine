9cc57d65f9f604d531b20d8313bd04776ec2bf19
trindadedev13 <trindadedev13@gmail.com>
2025-06-19 20:30:09 -0300

    chore(License): Use Apache 2.0

--- .scripts/replace_text.rb
+++ .scripts/replace_text.rb
@@ -11,6 +11,24 @@ directory = ARGV[0]
 old_text = ARGV[1]
 new_text = ARGV[2]
 
+# Check if old_text arg starts with @
+# if so try read text from file
+if old_text.start_with?('@')
+  old_text_filename = old_text[1..]
+  if File.exist?(old_text_filename)
+    old_text = File.read(old_text_filename)
+  end
+end
+
+# Check if new_text arg starts with @
+# if so try read text from file
+if new_text.start_with?('@')
+  new_text_filename = new_text[1..]
+  if File.exist?(new_text_filename)
+    new_text = File.read(new_text_filename)
+  end
+end
+
 excluded_dirs = ['.git', 'scripts']
 
 Find.find(directory) do |path|
